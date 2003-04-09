2ee568085fa7a8e621f0b940322a445d567e768f
Giorgio Metta <giorgio.metta@iit.it>
2003-04-09 11:00:46 +0000

    carlos modified a bunch of files for QNX6 there's a new flag __QNX6__ added new directories
    svn path=/trunk/yarp/; revision=219

---
+++ conf/YARPConfigTemplate.h
@@ -0,0 +1,10 @@
+//
+// $Id: YARPConfigTemplate.h,v 1.1 2003-04-09 11:00:46 gmetta Exp $
+// YARP config file (template).
+//
+//
+
+//
+// include here your config file and rename to YARPConfig.h
+
+#include <YARPConfigWin32.h>
