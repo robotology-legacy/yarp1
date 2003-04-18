029031c2a08b243fec966f020b55993c0ef9f873
Giorgio Metta <giorgio.metta@iit.it>
2003-04-18 13:46:03 +0000

    no message
    svn path=/trunk/yarp/; revision=248

--- src/libraries/ACE_wrappers/ace/config.h
+++ src/libraries/ACE_wrappers/ace/config.h
@@ -1,5 +1,5 @@
 //
-// $Id: config.h,v 1.3 2003-04-09 11:10:39 gmetta Exp $
+// $Id: config.h,v 1.4 2003-04-18 13:46:03 gmetta Exp $
 //	ACE Config File
 //
 
@@ -13,8 +13,17 @@
 
 #ifdef __WIN32__
 
+///#ifdef _MSC_VER
+///#error "_MSC_VER already defined"
+///#endif
+
+// my ACE configuration.
+///#define _MSC_VER 1200
 #define ACE_HAS_STANDARD_CPP_LIBRARY 1
 #include "ace/config-win32.h"
+
+// to ensure the object manager is static
+#undef ACE_HAS_NONSTATIC_OBJECT_MANAGER
 
 #endif
 
