223df52ba950a54125c62674c886560ff36f3c53
Lorenzo Natale <lorenzo.natale@iit.it>
2003-04-24 19:38:19 +0000

    just a typedef...
    svn path=/trunk/yarp/; revision=297

---
+++ src/libraries/motorcontrol/babybot/YARPBabybotHead.h
@@ -0,0 +1,11 @@
+#ifndef __YARPBABYBOTHEAD__
+#define __YARPBABYBOTHEAD__
+
+// $Id: YARPBabybotHead.h,v 1.1 2003-04-24 19:38:19 natta Exp $
+
+#include <YarpGenericComponent.h>
+#include "YARPMEIOnbabybotHeadAdapter.h"
+
+typedef YARPGenericComponent<YARPMEIOnBabybotHeadAdapter, YARPBabybotHeadParameters> YARPBabybotHead;
+
+#endif
