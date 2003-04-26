c73fd08fae3db70961a4ff663651c6ab6c6b3924
Lorenzo Natale <lorenzo.natale@iit.it>
2003-04-26 11:10:26 +0000

    yarpmeionbabybothead.h -> YARPMEIOnBabybotHeadAdapter.h
    svn path=/trunk/yarp/; revision=302

--- src/libraries/motorcontrol/babybot/YARPBabybotHead.h
+++ src/libraries/motorcontrol/babybot/YARPBabybotHead.h
@@ -1,10 +1,10 @@
 #ifndef __YARPBABYBOTHEAD__
 #define __YARPBABYBOTHEAD__
 
-// $Id: YARPBabybotHead.h,v 1.1 2003-04-24 19:38:19 natta Exp $
+// $Id: YARPBabybotHead.h,v 1.2 2003-04-26 11:10:26 natta Exp $
 
 #include <YarpGenericComponent.h>
-#include "YARPMEIOnbabybotHeadAdapter.h"
+#include "YARPMEIOnBabybotHeadAdapter.h"
 
 typedef YARPGenericComponent<YARPMEIOnBabybotHeadAdapter, YARPBabybotHeadParameters> YARPBabybotHead;
 
