6bf4e9b7f8b034ce9561e23bd6bcc640dca62aba
Lorenzo Natale <lorenzo.natale@iit.it>
2003-05-23 17:09:18 +0000

    utils lib created
    svn path=/trunk/yarp/; revision=436

---
+++ src/libraries/utils/include/YARPConfigFile.h
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//
+// very simple class to handle config files...
+
+// by nat May 2003
+//
+// $Id: YARPConfigFile.h,v 1.1 2003-05-23 17:09:18 natta Exp $
+
+#ifndef __YARPCONFIGFILE__
+#define __YARPCONFIGFILE__
+
+#include <conf/YARPConfig.h>
+#include <YARPErrorCodes.h>
+#include <string>
+
+////////////////////////////////////////////////////////////////////////////
+//
+class YARPConfigFile
+{
+public:
+	YARPConfigFile() {
+		_openFlag = false;
+	}
+
+	YARPConfigFile(const char *path) {
+		_openFlag = false;
+		_path = std::string(path);
+	}
+
+	YARPConfigFile(const char *path, const char *filename)
+	{
+		_path = std::string(path);
+		_filename = std::string(filename);
+		_openFlag = false;
+	}
+	
+	virtual ~YARPConfigFile(void)
+	{ _close(); };
+
+	int get(const char *section, const char *name, double *out, int n = 1);
+	int get(const char *section, const char *name, int *out, int n = 1);
+
+	void set(const char *path, const char *name)
+	{
+		setName(name);
+		setPath(path);
+	}
+
+	void setName(const char *name)
+	{ _filename = std::string(name); }
+	
+	void setPath(const char *path)
+	{ _path = std::string(path); }
+
+private:
+	std::string _path;
+	std::string _filename;
+
+	bool _open(const char *path, const char *filename);
+	void _close()
+	{
+		if (_openFlag) {
+			fclose(_pFile);
+			_openFlag = false;
+		}
+	}
+
+	int _get(const char *section, const char *name);
+	bool _findString(const char *str);
+	bool _findSection(const char *sec);
+
+	FILE *_pFile;
+	bool _openFlag;
+};
+
+#endif
---
+++ src/libraries/utils/src/YARPConfigFile.cpp
@@ -0,0 +1,108 @@
+//
+// YARPIniFile.cpp
+// $Id: YARPConfigFile.cpp,v 1.1 2003-05-23 17:09:18 natta Exp $
+
+#include "YARPConfigFile.h"
+
+//
+// gestione del path di default.
+//
+using namespace std;
+
+bool YARPConfigFile::_open(const char *path, const char *filename)
+{
+	std::string tmp = std::string(path) + std::string(filename);
+	_pFile = fopen(tmp.c_str(), "r");
+	if (_pFile != NULL)
+	{
+		_openFlag = true;
+		return true;
+	}
+	else 
+		return false;
+}
+
+int YARPConfigFile::get(const char *section, const char *name, double *out, int n)
+{
+	if (_get(section, name) == YARP_FAIL)
+		return YARP_FAIL;
+
+	int i = 0;
+	while ( (i<n) && (fscanf(_pFile, "%lf", out++) != EOF) )
+		i++;
+			
+	_close();
+	
+	if (i == n)
+		return YARP_OK;		// found all the values
+	else	
+		return YARP_FAIL;	
+}
+
+int YARPConfigFile::get(const char *section, const char *name, int *out, int n)
+{
+	if (_get(section, name) == YARP_FAIL)
+		return YARP_FAIL;
+
+	int i = 0;
+	while ( (i<n) && (fscanf(_pFile, "%d", out++) != EOF) )
+		i++;
+			
+	_close();
+	
+	if (i == n)
+		return YARP_OK;		// found all the values
+	else	
+		return YARP_FAIL;	
+}
+
+int YARPConfigFile::_get(const char *section, const char *name)
+{
+	if (!_open(_path.c_str(), _filename.c_str()))
+		return YARP_FAIL;
+
+	if (!_findSection(section))
+	{
+		_close();
+		return YARP_FAIL;
+	}
+
+	if (_findString(name))
+		return YARP_OK;
+	
+	_close();
+	return YARP_FAIL;
+}
+
+bool YARPConfigFile::_findSection(const char *sec)
+{
+	char row[255];
+	
+	while (fscanf(_pFile, "%s", row) != EOF)
+	{
+		if (strcmp(sec, row) == 0)
+			return true;
+	}
+
+	return false;
+}
+
+bool YARPConfigFile::_findString(const char *str)
+{
+	char row[255];
+
+	int l = strlen(str);
+	if (l == 0)
+		return false;
+	
+	while (fscanf(_pFile, "%s", row) != EOF)
+	{
+		if (row[0] == '[')
+			return false;		//end of section
+		
+		if (strncmp(row, str, l) == 0)
+			return true;
+	}
+
+	return false;
+}
