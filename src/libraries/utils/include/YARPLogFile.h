3c284d13d90e6b9a19b73c304ca670ca815c8f3d
YARP Developers <yarp0-devel@lists.sourceforge.net>
2003-05-30 21:46:07 +0000

    uhm...
    svn path=/trunk/yarp/; revision=466

---
+++ src/libraries/utils/include/YARPLogFile.h
@@ -0,0 +1,105 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//
+// very simple class to handle dump files...
+
+// by nat May 2003
+//
+// $Id: YARPLogFile.h,v 1.1 2003-05-30 21:46:07 babybot Exp $
+
+#ifndef __YARPDUMPFILE__
+#define __YARPDUMPFILE__
+
+#include <conf/YARPConfig.h>
+#include <YARPErrorCodes.h>
+#include <VisDMatrix.h>
+
+class YARPLogFile
+{
+public:
+	YARPLogFile();
+	YARPLogFile(const char *filename);
+	~YARPLogFile();
+	
+	int open(const char *filename)
+	{
+		if (filename == NULL)
+			return YARP_FAIL;
+
+		int n = strlen(filename);
+		close();		
+		
+		_name = new char [n+1];
+		strcpy(_name, filename);
+
+		_fp = fopen(filename,"w");
+
+		if (_fp != NULL)
+		{
+			_freeze = false;
+			return YARP_OK;
+		}
+		else
+			return YARP_FAIL;
+	}
+
+	int close()
+	{
+		if (_fp != NULL)
+			fclose(_fp);
+		if (_name != NULL)
+			delete [] _name;
+
+		_fp = NULL;
+		_name = NULL;
+
+		_freeze = true;
+		return YARP_OK;
+	}
+
+	inline int newLine()
+	{
+		if (_freeze)
+			return YARP_OK;
+
+		fprintf(_fp, "\n");
+		return YARP_OK;
+	}
+
+	inline int dump(const CVisDVector &v)
+	{
+		if (_freeze)
+			return YARP_OK;
+
+		int n = v.Length();
+		for(int i = 1; i <= n; i++)
+			fprintf(_fp, "%lf ", v(i));
+		
+		return YARP_OK;
+	}
+
+	inline int dump(double v)
+	{
+		if (_freeze)
+			return YARP_OK;
+
+		fprintf(_fp, "%lf ", v);
+		return YARP_OK;
+	}
+
+	inline int dump(int v)
+	{
+		if (_freeze)
+			return YARP_OK;
+
+		fprintf(_fp, "%d ", v);
+		return YARP_OK;
+	}
+
+private:
+	char *_name;
+	FILE *_fp;
+	bool _freeze;
+};
+
+#endif
