97c8ca4d37d31aee5bcb554ffffc9fdf6ffa7f24
Carlos Beltran-Gonzalez <carlos.beltran@iit.it>
2003-07-30 14:12:54 +0000

    This code could be the same for both setups. Only the adapter should change. For the moment use a local copy of babybotarm considering a inte gration in the near future
    svn path=/trunk/yarp/; revision=793

---
+++ src/libraries/motorcontrol/eurobot/YARPBabybotArm.cpp
@@ -0,0 +1,105 @@
+// $Id: YARPBabybotArm.cpp,v 1.1 2003-07-30 14:12:54 beltran Exp $
+
+#include "YARPBabybotArm.h"
+
+int YARPBabybotArm::setPositions(const double *pos)
+{
+	_lock();
+	angleToEncoders(pos, _temp_double);
+	for(int i = 0; i < _parameters._nj; i++)
+	{
+		SingleAxisParameters cmd;
+		cmd.axis = i;
+		cmd.parameters = &_temp_double[i];
+	
+		if (_parameters._stiffPID[i] == 1)
+			_adapter.IOCtl(CMDSetPosition, &cmd);
+	}
+	// _adapter.IOCtl(CMDSetPositions, _temp_double);
+	_unlock();
+	return -1;
+}
+
+int YARPBabybotArm::setPositionsAll(const double *pos)
+{
+	_lock();
+	angleToEncoders(pos, _temp_double);
+	 _adapter.IOCtl(CMDSetPositions, _temp_double);
+	_unlock();
+	return -1;
+}
+
+int YARPBabybotArm::setVelocities(const double *vel)
+{
+	_lock();
+	angleToEncoders(vel, _temp_double);
+	_adapter.IOCtl(CMDSetSpeeds, _temp_double);
+	_unlock();
+	return -1;
+}
+
+int YARPBabybotArm::setAccs(const double *acc)
+{
+	_lock();
+	angleToEncoders(acc, _temp_double);
+	_adapter.IOCtl(CMDSetAccelerations, _temp_double);
+	_unlock();
+	return -1;
+}
+
+int YARPBabybotArm::velocityMove(const double *vel)
+{
+	_lock();
+	angleToEncoders(vel, _temp_double);
+	_adapter.IOCtl(CMDVMove, _temp_double);
+	_unlock();
+	return -1;
+}
+
+int YARPBabybotArm::setCommands(const double *pos)
+{
+	_lock();
+	angleToEncoders(pos, _temp_double);
+	for(int i = 0; i < _parameters._nj; i++)
+	{
+		SingleAxisParameters cmd;
+		cmd.axis = i;
+		cmd.parameters = &_temp_double[i];
+		if (_parameters._stiffPID[i] != 1)
+			_adapter.IOCtl(CMDSetCommand, &cmd);
+	}
+	_unlock();
+	return -1;
+}
+
+int YARPBabybotArm::getPositions(double *pos)
+{
+	_lock();
+	_adapter.IOCtl(CMDGetPositions, _temp_double);
+	encoderToAngles(_temp_double, pos);
+	_unlock();
+	return -1;
+}
+
+int YARPBabybotArm::setG(int i, double g)
+{
+	_lock();
+	SingleAxisParameters cmd;
+	cmd.axis = _parameters._axis_map[i];
+	cmd.parameters = &g;
+	_adapter.IOCtl(CMDSetOffset, &cmd);
+	_unlock();
+	return -1;
+}
+
+int YARPBabybotArm::setGs(double *g, int nJ)
+{
+	_lock();
+	double offs[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+	for(int i = 0; i<nJ; i++)
+		offs[i] = *g;
+			
+	_adapter.IOCtl(CMDSetOffsets, offs);
+	_unlock();
+	return -1;
+}
