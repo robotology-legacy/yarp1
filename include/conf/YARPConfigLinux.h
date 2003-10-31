ed0f077308a5a79368bea6cb0af21e333f46345d
Giorgio Metta <giorgio.metta@iit.it>
2003-10-31 14:42:48 +0000

    no message
    svn path=/trunk/yarp/; revision=1061

---
+++ include/conf/YARPConfigLinux.h
@@ -0,0 +1,61 @@
+//
+// $Id: YARPConfigLinux.h,v 1.1 2003-10-31 14:42:48 gmetta Exp $
+// YARP config file WIN32.
+//
+//
+
+#ifndef __YARPConfigLinuxh__
+#define __YARPConfigLinuxh__
+
+//
+// included by YARPConfig.h
+
+#ifndef __LINUX__
+#	define __LINUX__
+#endif
+
+///
+/// Configuration flags
+///
+#define SINGLE_MUTEX_FOR_REFCOUNTED 1
+#define UPDATED_PORT 1
+#define MAX_PACKET (128*128*3+100)		
+///8192
+///(128*128*3+100)	/// shouldn't be required
+
+/// gcc doesn't have pragma once directive.
+///#define YARP_HAS_PRAGMA_ONCE	1
+
+/// disable TCP Nagle's algorithm (experimental).
+#define YARP_TCP_NO_DELAY		1
+
+/// set the default protocol in port creation.
+#define YARP_DEFAULT_PROTOCOL	YARP_TCP
+
+/// set here how many ports are requested by the def protocol.
+#define YARP_PROTOCOL_REGPORTS  11
+
+/// set the number of ports used by UDP for a single port.
+#define YARP_UDP_REGPORTS       11
+
+/// read timeout on socket transport (in seconds).
+#define YARP_SOCK_TIMEOUT       5
+
+/// read short timeout on socket transport (in seconds). - used for waiting short latency delay.
+#define YARP_SHORT_SOCK_TIMEOUT	1
+
+/// magic number for handshaking connections.
+#define YARP_MAGIC_NUMBER	7777
+
+/// alignment bytes required for mmx/ipl instructions.
+#define YARP_IMAGE_ALIGN 8
+
+/// default length for general purpose strings.
+#define YARP_STRING_LEN 256
+
+///
+/// ACE inclusion
+///
+#include <ace/config.h>
+
+#endif
