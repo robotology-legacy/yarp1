9dbb70f6803b80d3aacbbf32932346c00f3745fb
Carlos Beltran-Gonzalez <carlos.beltran@iit.it>
2003-11-13 18:22:41 +0000

    Implenting the head control under QNX
    svn path=/trunk/yarp/; revision=1120

--- src/libraries/motorcontrol/eurobot/YARPEurobotHead.h
+++ src/libraries/motorcontrol/eurobot/YARPEurobotHead.h
@@ -1,14 +1,113 @@
-#ifndef __YARPEURBOTHEAD__
+#ifndef __YARPEUROBOTHEAD__
 #define __YARPEUROBOTHEAD__
 
-// $Id: YARPEurobotHead.h,v 1.3 2003-09-08 16:59:00 beltran Exp $
+// $Id: YARPEurobotHead.h,v 1.4 2003-11-13 18:22:41 beltran Exp $
 
 #include <conf/YARPConfig.h>
 #include <YARPGenericControlBoard.h>
 #include "YARPGALILOnEurobotHeadAdapter.h"
 
-typedef YARPGenericControlBoard<YARPGALILOnEurobotHeadAdapter, YARPEurobotHeadParameters> YARPEurobotHead;
+namespace _limits
+{
+	const int neckPan = 0;
+	const int neckTilt = 1;
+	const int eyeTilt = 2;
+	const int eyeVersion = 3;
+	const int eyeVergence = 4;
+};
+
+namespace _joints
+{
+	const int neckPan = 0;
+	const int neckTilt = 1;
+	const int eyeTilt = 2;
+	const int rightEye = 3;
+	const int leftEye = 4;
+};
+
+//typedef YARPGenericControlBoard<YARPGALILOnEurobotHeadAdapter, YARPEurobotHeadParameters> YARPEurobotHead;
 
 //typedef YARPGALILOnEurobotHeadAdapter YARPEurobotHead;
+class YARPEurobotHead: public YARPGenericControlBoard<YARPGALILOnEurobotHeadAdapter, YARPEurobotHeadParameters>
+{
+public:
+	inline double vergence(double *pos)
+	{
+		return -( pos[_joints::rightEye] + pos[_joints::leftEye] );
+	}
+	inline double dot_vergence(double *cmd)
+	{
+		return cmd[_joints::rightEye] + cmd[_joints::leftEye];
+	}
+
+	inline double version(double *pos)
+	{
+		return 0.5*(pos[_joints::rightEye]-pos[_joints::leftEye]);
+	}
+	inline double dot_version(double *cmd)
+	{
+		return 0.5*(cmd[_joints::rightEye]-cmd[_joints::leftEye]);
+
+	}
+	inline bool checkLimits(double *pos, double *cmd)
+	{
+		bool ret = false;
+		bool tmp = false;
+	
+		// check eyes, vergence and version
+		tmp = checkSingleJoint(vergence(pos), dot_vergence(cmd), _parameters._limitsMax[_limits::eyeVergence], _parameters._limitsMin[_limits::eyeVergence], -1);
+		tmp |= checkSingleJoint(version(pos), dot_version(cmd), _parameters._limitsMax[_limits::eyeVersion], _parameters._limitsMin[_limits::eyeVersion], 1);
+		if (tmp)
+		{
+			cmd[_joints::rightEye] = 0.0;
+			cmd[_joints::leftEye] = 0.0;
+			ret = true;
+		}
+
+		// check eyes, tilt
+		tmp = checkSingleJoint(pos[_joints::eyeTilt], cmd[_joints::eyeTilt], _parameters._limitsMax[_limits::neckPan], _parameters._limitsMin[_limits::eyeTilt], 1);
+		if (tmp)
+		{
+			cmd[_joints::eyeTilt] = 0.0;
+			ret = true;
+		}
+
+		tmp = checkSingleJoint(pos[_joints::neckPan], cmd[_joints::neckPan], _parameters._limitsMax[_limits::neckPan], _parameters._limitsMin[_limits::neckPan], 1);
+		if (tmp)
+		{
+			cmd[_joints::neckPan] = 0.0;
+			ret = true;
+		}
+
+		tmp = checkSingleJoint(pos[_joints::neckTilt], cmd[_joints::neckTilt], _parameters._limitsMax[_limits::neckTilt], _parameters._limitsMin[_limits::neckTilt], 1);
+		if (tmp)
+		{
+			cmd[_joints::neckTilt] = 0.0;
+			ret = true;
+		}
+	
+		return ret;
+	}
+
+	// check limits on a sigle joints
+	inline bool checkSingleJoint(double pos, double cmd, double up, double low, int sign)
+	{
+		if (sign > 0)
+		{
+			if ((pos < low && cmd < 0) || (pos > up && cmd > 0))
+				return true;
+			else
+				return false;
+		}
+		else
+		{
+			if ((pos < low && cmd > 0) || (pos > up && cmd < 0))
+				return true;
+			else
+				return false;
+		}
+	}
+
+};
 
 #endif
--- src/libraries/motorcontrol/eurobot/YARPGALILOnEurobotHeadAdapter.h
+++ src/libraries/motorcontrol/eurobot/YARPGALILOnEurobotHeadAdapter.h
@@ -1,4 +1,4 @@
-//$Id: YARPGALILOnEurobotHeadAdapter.h,v 1.3 2003-10-17 16:34:40 babybot Exp $
+//$Id: YARPGALILOnEurobotHeadAdapter.h,v 1.4 2003-11-13 18:22:41 beltran Exp $
 
 #ifndef __GALILONEUROBOTHEAD__
 #define __GALILONEUROBOTHEAD__
@@ -6,6 +6,14 @@
 #include <ace/Log_Msg.h>
 #include <YARPGalilDeviceDriver.h>
 #include <string>
+#include <YARPConfigFile.h>
+
+#define YARP_BABYBOT_HEAD_ADAPTER_VERBOSE
+
+#ifdef YARP_BABYBOT_HEAD_ADAPTER_VERBOSE
+#define YARP_BABYBOT_HEAD_ADAPTER_DEBUG(string) YARP_DEBUG("BABYBOT_HEAD_ADAPTER_DEBUG:", string)
+#else  YARP_BABYBOT_HEAD_ADAPTER_DEBUG(string) YARP_NULL_DEBUG
+#endif
 
 namespace _EurobotHead
 {
@@ -23,7 +31,6 @@ namespace _EurobotHead
 
 	//ATENTION: the I_LIMIT and the T_LIMIT must be fixed later
 	const int _nj = 4;
-	char _mask = (char) 0x0F;
 	const LowLevelPID _highPIDs[_nj] =
 	{
 		LowLevelPID(5.0, 50.0, 0.08, 0.0, 0.0, 32767.0, 0.0, 32767.0, 0.0, 0.0),	//KP, KD, KI, AC_FF, VEL_FF, I_LIMIT, OFFSET, T_LIMIT, SHIFT, FRICT_FF
@@ -44,6 +51,8 @@ namespace _EurobotHead
 	const int _axis_map[_nj] = {3, 2, 1, 0};
 	const int _signs[_nj] = {0, 0, 0, 0}; //This must be fixed in the eurohead
 	const double _encoderToAngles[_nj] = {(2*pi)*89588.31, (2*pi)*23092.74, (2*pi)*22605.73, (2*pi)*95237.81}; 
+	const int _stiffPID[_nj] = {1, 1, 1, 1};
+	const double _maxDAC[_nj] = {9.9, 9.9, 9.9, 9.9};
 }; // namespace
 
 class YARPEurobotHeadParameters
@@ -51,8 +60,21 @@ class YARPEurobotHeadParameters
 public:
 	YARPEurobotHeadParameters()
 	{
+		_highPIDs = NULL;
+		_lowPIDs = NULL;
+		_zeros = NULL;
+		_signs = NULL;
+		_axis_map = NULL;
+		_encoderToAngles = NULL;
+		_stiffPID = NULL;
+		_maxDAC = NULL;
+		_limitsMax = NULL;
+		_limitsMin = NULL;
+		_nj = 0;
+
 		_nj = _EurobotHead::_nj;
-		_mask = _EurobotHead::_mask;
+		_mask = (char) 0x0F;
+		_realloc(_nj);
 		for(int i = 0; i<_nj; i++) {
 			_highPIDs[i] = _EurobotHead::_highPIDs[i];
 			_lowPIDs[i] = _EurobotHead::_lowPIDs[i];
@@ -63,18 +85,148 @@ public:
 		}
 	}
 
-	int load(const YARPString &path, const YARPString &init_file)
+	~YARPEurobotHeadParameters()
 	{
-		ACE_ASSERT(0);	//not implemented yet !
+		if (_highPIDs != NULL)
+			delete [] _highPIDs;
+		if (_lowPIDs != NULL)
+			delete [] _lowPIDs;
+		if (_zeros != NULL)
+			delete [] _zeros;
+		if (_signs != NULL)
+			delete [] _signs;
+		if (_axis_map != NULL)
+			delete [] _axis_map;
+		if (_encoderToAngles != NULL)
+			delete [] _encoderToAngles;
+		if (_stiffPID != NULL)
+			delete [] _stiffPID;
+		if (_maxDAC != NULL)
+			delete [] _maxDAC;
+		if (_limitsMax != NULL)
+			delete [] _limitsMax;
+		if (_limitsMin != NULL)
+			delete [] _limitsMin;
 	}
 
-	LowLevelPID _highPIDs[_EurobotHead::_nj];
-	LowLevelPID _lowPIDs[_EurobotHead::_nj];
-	double _zeros[_EurobotHead::_nj];
-	double _signs[_EurobotHead::_nj];
-	int _axis_map[_EurobotHead::_nj];
-	double _encoderToAngles[_EurobotHead::_nj];
+
+	int load(const YARPString &path, const YARPString &init_file)
+	{
+		YARPConfigFile cfgFile;
+		// set path and filename
+		cfgFile.set(path.c_str(), init_file.c_str());
+		
+		// get number of joints
+		if (cfgFile.get("[GENERAL]", "Joints", &_nj, 1) == YARP_FAIL)
+			return YARP_FAIL;
+
+		YARPString tmp;
+		if (cfgFile.getString("[GENERAL]", "InertialFile", tmp) == YARP_FAIL)
+			return YARP_FAIL;
+
+		_inertialConfig = path;
+		_inertialConfig.append(tmp);
+
+		// delete and allocate new memory
+		_realloc(_nj);
+		
+		int i;
+		for(i = 0; i<_nj; i++)
+		{
+			char dummy[80];
+			double tmp[12];
+			sprintf(dummy, "%s%d", "Pid", i);
+			if (cfgFile.get("[HIGHPID]", dummy, tmp, 12) == YARP_FAIL)
+				return YARP_FAIL;
+			_highPIDs[i] = LowLevelPID(tmp);
+			
+			if (cfgFile.get("[LOWPID]", dummy, tmp, 12) == YARP_FAIL)
+				return YARP_FAIL;
+			_lowPIDs[i] = LowLevelPID(tmp);
+		}
+		
+		if (cfgFile.get("[GENERAL]", "Zeros", _zeros, _nj) == YARP_FAIL)
+			return YARP_FAIL;
+		if (cfgFile.get("[GENERAL]", "AxisMap", _axis_map, _nj) == YARP_FAIL)
+			return YARP_FAIL;
+		if (cfgFile.get("[GENERAL]", "Signs", _signs, _nj) == YARP_FAIL)
+			return YARP_FAIL;
+		if (cfgFile.get("[GENERAL]", "MaxDAC", _maxDAC, _nj) == YARP_FAIL)
+			return YARP_FAIL;
+		if (cfgFile.get("[GENERAL]", "Stiff", _stiffPID, _nj) == YARP_FAIL)
+			return YARP_FAIL;
+
+		///////////////// HEAD LIMITS
+		if (cfgFile.get("[LIMITS]", "Max", _limitsMax, _nj) == YARP_FAIL)
+			return YARP_FAIL;
+		if (cfgFile.get("[LIMITS]", "Min", _limitsMin, _nj) == YARP_FAIL)
+			return YARP_FAIL;
+
+		// convert limits to radiants
+		for(i = 0; i < _nj; i++)
+		{
+			_limitsMax[i] = _limitsMax[i] * degToRad;
+			_limitsMin[i] = _limitsMin[i] * degToRad;
+		}
+		//////////////////////////////////////////////////////////////////
+
+		// build encoder to angles
+		if (cfgFile.get("[GENERAL]", "Encoder", _encoderToAngles, _nj) == YARP_FAIL)
+			return YARP_FAIL;
+		///////////////////////////////////////////////////
+
+		return YARP_OK;
+	}
+
+private:
+	void _realloc(int nj)
+	{
+		if (_highPIDs != NULL)
+			delete [] _highPIDs;
+		if (_lowPIDs != NULL)
+			delete [] _lowPIDs;
+		if (_zeros != NULL)
+			delete [] _zeros;
+		if (_signs != NULL)
+			delete [] _signs;
+		if (_axis_map != NULL)
+			delete [] _axis_map;
+		if (_encoderToAngles != NULL)
+			delete [] _encoderToAngles;
+		if (_stiffPID != NULL)
+			delete [] _stiffPID;
+		if (_maxDAC != NULL)
+			delete [] _maxDAC;
+		if (_limitsMax != NULL)
+			delete [] _limitsMax;
+		if (_limitsMin != NULL)
+			delete [] _limitsMin;
+		
+		_highPIDs = new LowLevelPID [nj];
+		_lowPIDs = new LowLevelPID [nj];
+		_zeros = new double [nj];
+		_signs = new double [nj];
+		_axis_map = new int [nj];
+		_encoderToAngles = new double [nj];
+		_limitsMax = new double [nj];
+		_limitsMin = new double [nj];
+		_stiffPID = new int [nj];
+		_maxDAC = new double [nj];
+	}
+
+public:
+	LowLevelPID *_highPIDs;
+	LowLevelPID *_lowPIDs;
+	double *_zeros;
+	double *_signs;
+	int *_axis_map;
+	double *_encoderToAngles;
+	int *_stiffPID;
 	int _nj;
+	double *_maxDAC;
+	YARPString _inertialConfig;
+	double *_limitsMax;
+	double *_limitsMin;
 	char _mask;
 };
 
@@ -92,14 +244,6 @@ public:
 			uninitialize();
 	}
 	
-	/*
-	int initialize()
-	{
-		YARPEurobotHeadParameters parameters;
-		initialize((const YARPEurobotHeadParameters)parameters);
-	}
-	*/
-
 	int initialize(YARPEurobotHeadParameters *par)
 	{
 		_parameters = par;
@@ -183,6 +327,58 @@ public:
 			//IOCtl(CMDEnableAmp, &i);
 			//IOCtl(CMDClearStop, &i);
 		}
+		return YARP_OK;
+	}
+
+	int readAnalogs(double *val)
+	{
+		
+		int i;
+		int ret;
+
+		//This should be included in the low level device driver
+		/**
+		SingleAxisParameters cmd;
+		for(i = 0; i < _inertial->_ns; i++)
+		{
+			cmd.axis = i;
+			cmd.parameters = &_tmpShort[i];;
+			ret = IOCtl(CMDReadAnalog, &cmd);
+		}
+		_inertial->convert(_tmpShort, val);
+		**/
+		return ret;
+	}
+
+	int calibrate()
+	{
+		YARP_BABYBOT_HEAD_ADAPTER_DEBUG(("Starting head calibration routine"));
+		ACE_OS::printf("..done!\n");
+	/*
+
+		YARP_BABYBOT_HEAD_ADAPTER_DEBUG(("Starting inertial sensor calibration:\n"));
+
+		int p = 0;
+		bool cal = false;
+		while (!cal)
+		{
+			// read new values from analog input
+			int i;
+			int ret;
+			SingleAxisParameters cmd;
+			for(i = 0; i < _inertial->_ns; i++)
+			{
+				cmd.axis = i;
+				cmd.parameters = &_tmpShort[i];;
+				ret = IOCtl(CMDReadAnalog, &cmd);
+			}
+
+			cal = _inertial->calibrate(_tmpShort, &p);
+			ACE_OS::printf("%d\r",p);
+			ACE_OS::sleep(ACE_Time_Value(0,40000));
+		}
+		ACE_OS::printf("\n..done!\n");
+	*/
 		return YARP_OK;
 	}
 
