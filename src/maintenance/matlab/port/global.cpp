d932f75c396ebf32dc39257cf4e615a7e69a5ed6
Giorgio Metta <giorgio.metta@iit.it>
2003-11-13 22:18:05 +0000

    added matlab interface to port files not completed, not tested, supposedly platform independent only minimally tested on winnt same code should be recompilable under linux at least mex file interface
    svn path=/trunk/yarp/; revision=1121

---
+++ src/maintenance/matlab/port/global.cpp
@@ -0,0 +1,485 @@
+/////////////////////////////////////////////////////////////////////////
+///                                                                   ///
+///                                                                   ///
+/// This Academic Free License applies to any software and associated ///
+/// documentation (the "Software") whose owner (the "Licensor") has   ///
+/// placed the statement "Licensed under the Academic Free License    ///
+/// Version 1.0" immediately after the copyright notice that applies  ///
+/// to the Software.                                                  ///
+/// Permission is hereby granted, free of charge, to any person       ///
+/// obtaining a copy of the Software (1) to use, copy, modify, merge, ///
+/// publish, perform, distribute, sublicense, and/or sell copies of   ///
+/// the Software, and to permit persons to whom the Software is       ///
+/// furnished to do so, and (2) under patent claims owned or          ///
+/// controlled by the Licensor that are embodied in the Software as   ///
+/// furnished by the Licensor, to make, use, sell and offer for sale  ///
+/// the Software and derivative works thereof, subject to the         ///
+/// following conditions:                                             ///
+/// Redistributions of the Software in source code form must retain   ///
+/// all copyright notices in the Software as furnished by the         ///
+/// Licensor, this list of conditions, and the following disclaimers. ///
+/// Redistributions of the Software in executable form must reproduce ///
+/// all copyright notices in the Software as furnished by the         ///
+/// Licensor, this list of conditions, and the following disclaimers  ///
+/// in the documentation and/or other materials provided with the     ///
+/// distribution.                                                     ///
+///                                                                   ///
+/// Neither the names of Licensor, nor the names of any contributors  ///
+/// to the Software, nor any of their trademarks or service marks,    ///
+/// may be used to endorse or promote products derived from this      ///
+/// Software without express prior written permission of the Licensor.///
+///                                                                   ///
+/// DISCLAIMERS: LICENSOR WARRANTS THAT THE COPYRIGHT IN AND TO THE   ///
+/// SOFTWARE IS OWNED BY THE LICENSOR OR THAT THE SOFTWARE IS         ///
+/// DISTRIBUTED BY LICENSOR UNDER A VALID CURRENT LICENSE. EXCEPT AS  ///
+/// EXPRESSLY STATED IN THE IMMEDIATELY PRECEDING SENTENCE, THE       ///
+/// SOFTWARE IS PROVIDED BY THE LICENSOR, CONTRIBUTORS AND COPYRIGHT  ///
+/// OWNERS "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, ///
+/// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   ///
+/// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO      ///
+/// EVENT SHALL THE LICENSOR, CONTRIBUTORS OR COPYRIGHT OWNERS BE     ///
+/// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   ///
+/// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN ///
+/// CONNECTION WITH THE SOFTWARE.                                     ///
+///                                                                   ///
+/// This license is Copyright (C) 2002 Lawrence E. Rosen. All rights  ///
+/// reserved. Permission is hereby granted to copy and distribute     ///
+/// this license without modification. This license may not be        ///
+/// modified without the express written permission of its copyright  ///
+/// owner.                                                            ///
+///                                                                   ///
+///                                                                   ///
+/////////////////////////////////////////////////////////////////////////
+
+///
+///
+///       YARP - Yet Another Robotic Platform (c) 2001-2003 
+///
+///                    #pasa#
+///
+///     "Licensed under the Academic Free License Version 1.0"
+///
+
+///
+/// $Id: global.cpp,v 1.1 2003-11-13 22:18:05 gmetta Exp $
+///
+///
+
+///
+/// I store here my own global stuff, in particular a list of ports and their attributes.
+/// - I expect to recast ports to their original types for using them
+/// - This memory is not deallocated unless matlab issues a clean unloading the DLL.
+///
+
+#include <conf/YARPConfig.h>
+#include <ace/config.h>
+#include <ace/OS.h>
+
+#include <YARPPort.h>
+#include <YARPSemaphore.h>
+#include <YARPThread.h>
+
+/// requires image management abilities as well as math stuff (vectors).
+///#include <>
+
+#include "global.h"
+
+static _portEntry _listOfEntries[MAX_PORTS] =
+{ 
+	{ 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL },
+	{ 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL },
+	{ 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL },
+	{ 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL },
+	{ 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL },
+	{ 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL },
+	{ 0, MX_YARP_INVALID, false, NULL }, { 0, MX_YARP_INVALID, false, NULL }
+};
+
+///
+///
+/// local internal prototypes.
+int stringToProtocol (const char *s);
+char *typeToString(enum _dataType type, char *buffer);
+
+/// operation callbacks.
+int create_port (void *params);
+int register_port (void *params);
+int unregister_port (void *params);
+int destroy_port (void *params);
+int list_port (void *params);
+ 
+///
+///
+/// a list of pointers to functions.
+typedef int (*PV) (void *);
+
+struct _actionEntryTag
+{
+	PV _function;
+	char *_operation;
+};
+typedef struct _actionEntryTag _actionEntry;
+
+const int NUM_OF_ACTIONS = 5;
+static _actionEntry _actions[NUM_OF_ACTIONS] =
+{
+	{ (PV)create_port, "create" },
+	{ (PV)register_port, "register" },
+	{ (PV)unregister_port, "unregister" },
+	{ (PV)destroy_port, "destroy" },
+	{ (PV)list_port, "list" }
+};
+
+///
+int stringToProtocol (const char *s)
+{
+	if (strncmp (s, "tcp", 3) == 0)
+		return YARP_TCP;
+	else
+	if (strncmp (s, "udp", 3) == 0)
+		return YARP_UDP;
+	else
+	if (strncmp (s, "mcast", 4) == 0)
+		return YARP_MCAST;
+	else
+		/// default.
+		return YARP_TCP;
+}
+
+/// from the yarp protocol enumeration.
+///	YARP_UDP					= 0,
+///	YARP_TCP					= 1,
+///	YARP_MCAST					= 2,
+///	YARP_QNET					= 3,
+/// YARP_SHMEM					= 4,
+
+int dispatcher (const char *operation, void *params)
+{
+	for (int i = 0; i < NUM_OF_ACTIONS; i++)
+	{
+		if (strcmp (operation, _actions[i]._operation) == 0)
+			return (*_actions[i]._function)(params);
+	}
+
+	return -1;
+}
+
+
+/*
+	also from yarp, the buffering
+
+	input port:
+		NO_BUFFERS,
+		DOUBLE_BUFFERS,
+		TRIPLE_BUFFERS,
+		DEFAULT_BUFFERS = TRIPLE_BUFFERS
+
+	output port:
+		MANY_OUTPUTS,
+		SINGLE_OUTPUT,
+		DEFAULT_OUTPUTS = MANY_OUTPUTS
+*/
+
+///
+///
+/// messing up things here :)
+///
+#define PORT_CREATE(x, y) \
+		case MX_YARP_##x: \
+		{ \
+				if (par._direction) \
+					myport._port = (void *) new YARPInputPortOf<##y> (YARPInputPort::DEFAULT_BUFFERS, stringToProtocol(par._protocol)); \
+				else \
+					myport._port = (void *) new YARPOutputPortOf<##y> (YARPOutputPort::DEFAULT_OUTPUTS, stringToProtocol(par._protocol)); \
+				myport._type = MX_YARP_INT; \
+				myport._direction = par._direction; \
+				myport._name = NULL; \
+		} \
+		break;
+
+///
+///
+#define PORT_REGISTER(x, y) \
+  	case MX_YARP_##x: \
+		{ \
+			if (myport._direction) \
+			{ \
+				YARPInputPortOf<##y> *port = (YARPInputPortOf<##y> *)myport._port; \
+				return_value = port->Register (par._portname, par._network); \
+				if (return_value >= 0) \
+				{ \
+					int len = strlen(par._portname); \
+					myport._name = new char[len+1]; \
+					strcpy (myport._name, par._portname); \
+					myport._name[len] = 0; \
+				} \
+			} \
+			else \
+			{ \
+				YARPOutputPortOf<int> *port = (YARPOutputPortOf<int> *)myport._port; \
+				return_value = port->Register (par._portname, par._network); \
+				if (return_value >= 0) \
+				{ \
+					int len = strlen(par._portname); \
+					myport._name = new char[len+1]; \
+					strcpy (myport._name, par._portname); \
+					myport._name[len] = 0; \
+				} \
+			} \
+		} \
+	break;
+
+///
+///
+#define PORT_UNREGISTER(x, y) \
+	case MX_YARP_##x: \
+		{ \
+			if (myport._direction) \
+			{ \
+				YARPInputPortOf<##y> *port = (YARPInputPortOf<##y> *)myport._port; \
+				return_value = port->Unregister (); \
+				if (myport._name != NULL) delete[] myport._name; \
+				myport._name = NULL; \
+			} \
+			else \
+			{ \
+				YARPOutputPortOf<##y> *port = (YARPOutputPortOf<##y> *)myport._port; \
+				return_value = port->Unregister (); \
+				if (myport._name != NULL) delete[] myport._name; \
+				myport._name = NULL; \
+			} \
+		} \
+		break; \
+
+///
+///
+#define PORT_DESTROY(x, y) \
+	case MX_YARP_##x: \
+		{ \
+			if (myport._direction) \
+			{ \
+				YARPInputPortOf<##y> *port = (YARPInputPortOf<##y> *)myport._port; \
+				delete port; \
+				myport._port = NULL; \
+				myport._type = MX_YARP_INVALID; \
+				myport._direction = false; \
+				myport._name = NULL; \
+			} \
+			else \
+			{ \
+				YARPOutputPortOf<##y> *port = (YARPOutputPortOf<##y> *)myport._port; \
+				delete port; \
+				myport._port = NULL; \
+				myport._type = MX_YARP_INVALID; \
+				myport._direction = false; \
+				myport._name = NULL; \
+			} \
+		} \
+		break;
+
+///
+///
+///
+
+///
+///
+/// create a port of the given type and stores into list.
+int create_port (void *params)
+{
+	_dispatchParams& par = *(_dispatchParams *)params;
+	int i, portno = -1;
+
+	for (i = 0; i < MAX_PORTS; i++)
+	{
+		if (_listOfEntries[i]._port == NULL)
+		{
+			portno = i;
+			break;
+		}
+	}
+
+	if (portno == -1)
+		return -1;
+
+	///
+	_portEntry& myport = _listOfEntries[portno];
+
+	switch (par._type)
+	{
+		PORT_CREATE (INT, int);
+		PORT_CREATE (DOUBLE, double);
+
+		default:
+			return -1;
+			break;
+	}
+
+	return portno;
+}
+
+///
+///
+///
+int register_port (void *params)
+{
+	_dispatchParams& par = *(_dispatchParams *)params;
+	int portno = -1;
+	int return_value = -1;
+
+	portno = par._portnumber;
+	if (portno < 0 || portno >= MAX_PORTS)
+		return -1;
+
+	_portEntry& myport = _listOfEntries[portno];
+	if (par._portname == NULL ||
+		par._network == NULL ||
+		myport._name != NULL)	/// already registered.
+		return -1;
+
+	switch (myport._type)
+	{
+		PORT_REGISTER (INT, int);
+		PORT_REGISTER (DOUBLE, double);
+
+		default:
+			return -1;
+			break;
+	}
+
+	return return_value;
+}
+
+///
+///
+///
+int unregister_port (void *params)
+{
+	_dispatchParams& par = *(_dispatchParams *)params;
+	int portno = -1;
+	int return_value = -1;
+
+	portno = par._portnumber;
+	if (portno < 0 || portno >= MAX_PORTS)
+		return -1;
+
+	_portEntry& myport = _listOfEntries[portno];
+	if (myport._name == NULL)
+		return -1;
+
+	switch (myport._type)
+	{
+		PORT_UNREGISTER(INT, int);
+		PORT_UNREGISTER(DOUBLE, double);
+
+		default:
+			return -1;
+			break;
+	}
+
+	return return_value;
+}
+
+///
+///
+///
+int destroy_port (void *params)
+{
+	_dispatchParams& par = *(_dispatchParams *)params;
+	int portno = -1;
+	int return_value = 0;
+
+	portno = par._portnumber;
+	if (portno < 0 || portno >= MAX_PORTS)
+		return -1;
+
+	_portEntry& myport = _listOfEntries[portno];
+
+	if (myport._name != NULL)
+	{
+		int r = unregister_port (params);
+		if (r < 0)
+			return r;
+	}
+
+	switch (myport._type)
+	{
+		PORT_DESTROY (INT, int);
+		PORT_DESTROY (DOUBLE, double);
+
+		default:
+			return -1;
+			break;
+	}
+
+	return return_value;
+}
+
+///
+///
+///
+char *typeToString(enum _dataType type, char *buffer)
+{
+	switch (type)
+	{
+	case MX_YARP_INT:
+		strncpy (buffer, "int", 3);
+		buffer[3] = 0;
+		break;
+
+	case MX_YARP_DOUBLE:
+		strncpy (buffer, "double", 6);
+		buffer[6] = 0;
+		break;
+
+	case MX_YARP_YVECTOR:
+		strncpy (buffer, "vector", 6);
+		buffer[6] = 0;
+		break;
+
+	case MX_YARP_IMAGE:
+		strncpy (buffer, "image", 5);
+		buffer[5] = 0;
+		break;
+
+	default:
+		strncpy (buffer, "invalid type", 12);
+		buffer[12] = 0;
+		break;
+	}
+
+	return buffer;
+}
+
+///
+///
+/// list active ports.
+int list_port (void *params)
+{
+	int i;
+	_dispatchParams& par = *(_dispatchParams *)params;
+
+	if (par._extra_content == NULL)
+		return -1;
+
+	char *tmp = par._extra_content;
+	sprintf (tmp, "list of active ports\n");
+	tmp = par._extra_content + strlen (par._extra_content);
+
+	char buffer[25];
+
+	for (i = 0; i < MAX_PORTS; i++)
+	{
+		_portEntry& myport = _listOfEntries[i];
+		if (myport._port != NULL)
+		{
+			sprintf (tmp, "$$ %d of type %s of %s, registered as %s\n", i, typeToString(myport._type, buffer), (myport._direction)?"input":"output", (myport._name != NULL)?myport._name:"(Null)");
+		}
+		else
+		{
+			sprintf (tmp, "$$ slot %d empty\n", i);
+		}
+
+		tmp = par._extra_content + strlen (par._extra_content);
+	}
+	return 0;
+}
---
+++ src/maintenance/matlab/port/readme.txt
@@ -0,0 +1,15 @@
+MATLAB Project Wizard Wizard will generate a project and will 
+request you to pick up a top level file to add to the project.
+You can later use the M++ button from the MATLAB Visual Studio 
+Addin to add more files to the project.
+
+The project contains:
+ 
+port.c		Main C file 
+port.dsw		MSVC Developer Studio Workspace
+port.dsp		MSVC Developer Studio Project File
+readme.txt	This text
+
+To debug DLLs (MEX files or Simulink S-Functions) specify MATLAB.EXE
+(e.g c:\MatlabR12\bin\win32\matlab.exe) as the Executable for Debug
+Session.
