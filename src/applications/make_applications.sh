7e759695890c8a563ff70aa5f9173fc3cd08a4ce
Carlos Beltran-Gonzalez <carlos.beltran@iit.it>
2003-12-17 17:19:02 +0000

    Script for the applications
    svn path=/trunk/yarp/; revision=1260

---
+++ src/applications/make_applications.sh
@@ -0,0 +1,73 @@
+#! /bin/sh
+
+#
+# this is a global script file for compiling YARP applications
+# options
+#		<debug> compile DEBUG
+#		<release> compile RELEASE, optimization ON
+#		<clean>	clean everything, be careful.
+#
+#
+
+EXECUTABLES="./viewers/
+			 ./misc/"	
+
+All()
+{
+	for execut in $EXECUTABLES
+	do
+		echo "$PHRASE $execut"
+		cd $execut
+		make $MODE
+		Check $?
+		$INSTALL
+		Check $?
+		cd $YARP_ROOT/src/applications/ 
+	done
+}
+
+Check()
+{
+	if [ $1 -eq 0 ]         # Test exit status of "cmp" command.
+	then
+		echo "COMMAND SUCCESSFUL"
+	else  
+		echo "ERROR DETECTED"
+		exit 0
+	fi
+}
+
+Nothing()
+{
+	echo "A parameter is needed (debug, release, clean)"
+}
+
+echo "Entering build process of YARP libraries..."
+
+if [ "$1" ==  "" ]
+then
+	Nothing
+elif [ "$1" == "debug" ]
+then
+	echo "debug"
+	MODE="debug"
+	PHRASE="Building (debug)"
+	#install option
+	INSTALL="make install"
+	All 
+elif [ "$1" == "release" ]
+then
+	echo "release"
+	MODE=""
+	PHRASE="Building (release)"
+	#install
+	INSTALL="make install"
+	All 
+elif [ "$1" == "clean" ]
+then
+	echo "clean"
+	MODE="clean"
+	PHRASE="Cleaning"
+	INSTALL=""
+	All 
+fi
