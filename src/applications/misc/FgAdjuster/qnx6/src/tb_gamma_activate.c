d27e52c73d54cddc6e323e0241aa9e2a1ab13de7
Carlos Beltran-Gonzalez <carlos.beltran@iit.it>
2004-01-21 20:24:27 +0000

    New callbacks for new filters
    svn path=/trunk/yarp/; revision=1340

---
+++ src/applications/misc/FgAdjuster/qnx6/src/tb_gamma_activate.c
@@ -0,0 +1,43 @@
+// =====================================================================================
+//
+//       YARP - Yet Another Robotic Platform (c) 2001-2003 
+//
+//                    #Ing. Carlos Beltran#
+//
+//     "Licensed under the Academic Free License Version 1.0"
+// 
+//        Filename:  tb_gamma_activate.c
+// 
+//     Description:  
+// 
+//         Version:  $Id: tb_gamma_activate.c,v 1.1 2004-01-21 20:24:27 beltran Exp $
+// 
+//          Author:  Ing. Carlos Beltran (Carlos), cbeltran@dist.unige.it
+//         Company:  Lira-Lab
+// 
+// =====================================================================================
+
+/* Standard headers */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/* Local headers */
+#include "ablibs.h"
+#include "abimport.h"
+#include "proto.h"
+extern int _gamma;
+
+int
+tb_gamma_activate( PtWidget_t *widget, ApInfo_t *apinfo, PtCallbackInfo_t *cbinfo )
+{
+	PtBasicCallback_t * ptbcb;
+	/* eliminate 'unreferenced' warnings */
+	widget = widget, apinfo = apinfo, cbinfo = cbinfo;
+	ptbcb = (PtBasicCallback_t *)cbinfo->cbdata;	
+	_gamma = ptbcb->value;
+	sendfginfo();
+
+	return( Pt_CONTINUE );
+}
