0e1722fa7c4ec8ddd98b2879658245228c4aa690
YARP Developers <yarp0-devel@lists.sourceforge.net>
2004-01-29 17:44:28 +0000

    obsolete
    svn path=/trunk/yarp/; revision=1362

--- scripts/babybot/armrandomrun.js
+++
@@ -1,5 +0,0 @@
-var WshShell = WScript.CreateObject("WScript.Shell");
-WshShell.Run ("on -n proteus armcontrol.exe", 1, true);
-WshShell.Run ("behaviorrnd.exe", 1, true);
-WshShell.Run ("sendcmd.exe", 1, true);
-WshShell.Run ("repeater.exe -name repeater/", 1, true);
