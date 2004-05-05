8045f0beddc5b1a7622cab83d07eb54371bc30f0
YARP Developers <yarp0-devel@lists.sourceforge.net>
2004-05-05 15:20:15 +0000

    no message
    svn path=/trunk/yarp/; revision=1527

--- scripts/babybot/visualAttentionConnect.js
+++ scripts/babybot/visualAttentionConnect.js
@@ -1,13 +1,9 @@
 var WshShell = WScript.CreateObject("WScript.Shell");
 
 WshShell.Run ("porter.exe /left/o:img !/visualattention/left/i:img", 7, true);
-WshShell.Run ("porter.exe /right/o:img !/visualattention/right/i:img", 7, true);
 
-WshShell.Run ("porter.exe /visualattention/left/o:img !/visualattention/view/right/i:img", 7, true);
-WshShell.Run ("porter.exe /visualattention/right/o:img !/visualattention/view/left/i:img", 7, true);
+WshShell.Run ("porter.exe /visualattention/left/o:img !/visualattention/view/left/i:img", 7, true);
 
 WshShell.Run ("porter.exe /left/o:img /visualattention/left/i:img", 7, true);
-WshShell.Run ("porter.exe /right/o:img /visualattention/right/i:img", 7, true);
 
-WshShell.Run ("porter.exe /visualattention/left/o:img /visualattention/view/right/i:img", 7, true);
-WshShell.Run ("porter.exe /visualattention/right/o:img /visualattention/view/left/i:img", 7, true);
+WshShell.Run ("porter.exe /visualattention/left/o:img /visualattention/view/left/i:img", 7, true);
--- scripts/babybot/visualAttentionSlay.js
+++ scripts/babybot/visualAttentionSlay.js
@@ -1,4 +1,4 @@
 var WshShell = WScript.CreateObject("WScript.Shell");
 
-WshShell.Run ("slay -n leto visualattention.exe", 7, true);
+WshShell.Run ("slay -n scilla visualattention.exe", 7, true);
 WshShell.Run ("slay -n calliope camview.exe", 7, true);
