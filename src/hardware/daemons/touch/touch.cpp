800507fc7327a19d6086dee9f83215f3a9e9f43c
YARP Developers <yarp0-devel@lists.sourceforge.net>
2004-07-15 17:45:52 +0000

    median filter on touch sensor daemon
    svn path=/trunk/yarp/; revision=1744

--- src/hardware/daemons/touch/touch.cpp
+++ src/hardware/daemons/touch/touch.cpp
@@ -25,6 +25,7 @@ const int __defaultRate = 40;
 const YARPString __defaultName = "/touch/o";
 const int __nSensors = 17;
 
+// we keep a history of 3 elements and we apply a median filter
 class MyThread : public YARPRateThread
 {
 public:
@@ -34,6 +35,14 @@ public:
 		_name = YARPString(name);
 		_readings.Resize(__nSensors);
 		_readings = 0.0;
+
+		int i;
+		for(i = 0; i < 3; i++)
+		{
+			_history[i].Resize(__nSensors);
+			_history[i] = 0.0;
+		}
+		_index = 0;
 	}
 	~MyThread()
 	{
@@ -50,11 +59,16 @@ public:
 	void doLoop()
 	{
 	
-		_touchBoard.read(_readings.data());
+		_touchBoard.read(_history[_index].data());
+		_filter();
 
 		_outPort.Content() = _readings;
 				
 		_outPort.Write();
+
+		_index++;
+		if (_index == 3)
+			_index = 0;
 	}
 
 	void doRelease()
@@ -62,11 +76,43 @@ public:
 		_touchBoard.uninitialize();
 	}
 
+	void _filter()
+	{
+		double tmp[3];
+		for(int j = 1; j<=__nSensors; j++)
+		{
+			tmp[0] = _history[0](j);
+			tmp[1] = _history[1](j);
+			tmp[2] = _history[2](j);
+
+			sort(tmp);
+			_readings(j) = tmp[1];	
+		}
+	}
+
+	// bubble sort with 3 elements
+	void sort(double *a)
+	{
+		int j; int i;
+		double tmp;
+		for (i=0; i<3; i++)
+		{
+			for (j=0; j<2; j++)
+				if (a[j+1] < a[j])
+				{  /* compare the two neighbors */
+					tmp = a[j];         /* swap a[j] and a[j+1]      */
+					a[j] = a[j+1];
+					a[j+1] = tmp;
+				}
+			}
+	}
 	
 	YARPTouchBoard _touchBoard;
 	YARPOutputPortOf<YVector> _outPort;
+	YVector _history[3];
 	YVector _readings;
 	YARPString _name;
+	int _index;
 };
 
 
@@ -79,6 +125,8 @@ int main(int argc, char* argv[])
 	YARPString name;
 	if (!YARPParseParameters::parse(argc, argv, "p", &rate))
 		rate = __defaultRate;
+
+	printf("Using rate: %d\n", rate);
 
 	if (!YARPParseParameters::parse(argc, argv, "name", name))
 		name = __defaultName;
