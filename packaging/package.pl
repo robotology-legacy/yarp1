834ab85a62549f8d8374f7381f9a5903289d34a4
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2004-07-29 14:10:24 +0000

    tweaks
    svn path=/trunk/yarp/; revision=1813

--- packaging/package.pl
+++ packaging/package.pl
@@ -57,48 +57,51 @@ sub getInCVS {
 
 while (<>) {
     chomp($_);
+    $_ =~ s/\#.*//;
     print "command: $_\n";
     my @args = split(/[ \t]+/, $_);
-    my $cmd = $args[0];
-    if ($cmd eq "package") {
-	$name = $args[1];
-	print "Creating package [$name]\n";
-	mkpath($name);
-    } elsif ($cmd eq "copy") {
+    if ($#args>=0) {
+	my $cmd = $args[0];
+	if ($cmd eq "package") {
+	    $name = $args[1];
+#	    print "Creating package [$name]\n";
+	    mkpath($name);
+	} elsif ($cmd eq "copy") {
 #	my $target = $name . "/" . $args[1];
 #	my $source = $root . "/" . $args[1];
-	my $mode = $args[1];
-	my $source = $args[2];
-	print "source [$source]\n";
-	my $cwd = cwd();
-	chdir($root);
-	print "In directory ", cwd(), "\n";
-	my @inventory;
-	if ($mode =~ /file/i) {
-	    @inventory = ($source);
-	} else {
-	    @inventory = getInCVS($source);
-	}
-	chdir($cwd);
-	foreach my $f (@inventory) {
-	    my $f1 = $root . '/' . $f;
-	    my $f2 = $cwd . '/' . $name . '/' . $f;
-	    print ">>> copy $f1 $f2\n";
-	    my $dir = $f2;
-	    $dir =~ s:/[^/]*$::;
-	    print ">>> copy $f1 $f2 (dir $dir)\n";
-	    mkpath($dir);
-	    copy($f1,$f2) or die "Copy failed: $!";
-	}
+	    my $mode = $args[1];
+	    my $source = $args[2];
+#	    print "source [$source]\n";
+	    my $cwd = cwd();
+	    chdir($root);
+#	    print "In directory ", cwd(), "\n";
+	    my @inventory;
+	    if ($mode =~ /file/i) {
+		@inventory = ($source);
+	    } else {
+		@inventory = getInCVS($source);
+	    }
+	    chdir($cwd);
+	    foreach my $f (@inventory) {
+		my $f1 = $root . '/' . $f;
+		my $f2 = $cwd . '/' . $name . '/' . $f;
+		#print ">>> copy $f1 $f2\n";
+		my $dir = $f2;
+		$dir =~ s:/[^/]*$::;
+		#print ">>> copy $f1 $f2 (dir $dir)\n";
+		mkpath($dir);
+		copy($f1,$f2) or die "Copy failed ($f1 to $f2): $!";
+	    }
 #	mkpath($target,1);
 #	print "Source is $source\n";
 #	my $cwd = cwd();
 #	chdir($root);
 #	find(\&show,($source));
 #	chdir($cwd);
-    } elsif ($cmd eq "root") {
-	$root = $args[1];
-	print "Setting root directory to [$root]\n";
+	} elsif ($cmd eq "root") {
+	    $root = $args[1];
+	    #print "Setting root directory to [$root]\n";
+	}
     }
 }
 
