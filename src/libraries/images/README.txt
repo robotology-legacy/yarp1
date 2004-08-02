891797ec2f4c2d147a979f4909ef6460aa07bd54
YARP Developers <yarp0-devel@lists.sourceforge.net>
2004-08-02 12:50:33 +0000

    removed a bunch of old projects and directories
    svn path=/trunk/yarp/; revision=1862

--- src/libraries/images/README.txt
+++
@@ -1,2 +0,0 @@
-Have a look at %YARP_INCLUDE%\conf\install\OS\install.txt
-	where OS is you OS dir name.
--- src/libraries/ipl/install_ipl.bat
+++
@@ -1,7 +0,0 @@
-@echo installing ipl...
-
-copy .\include\*.h ..\..\..\include\sys
-copy .\lib\*.lib ..\..\..\lib\winnt
-copy .\bin\*.dll ..\..\..\bin\winnt
-
-@echo done!
