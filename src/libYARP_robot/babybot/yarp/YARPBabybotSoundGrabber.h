6df9d461713714e8e17fbf1cc4728d09b5b542ed
Carlos Beltran-Gonzalez <carlos.beltran@iit.it>
2004-08-30 13:06:56 +0000

    Fixed final name of YARPBabybotSoundGrabber. Now the name is beeing mapped by the perl script into YARPRobotHardware.h
    svn path=/trunk/yarp/; revision=1934

--- src/libYARP_robot/babybot/yarp/YARPBabybotSoundGrabber.h
+++ src/libYARP_robot/babybot/yarp/YARPBabybotSoundGrabber.h
@@ -1,34 +1,41 @@
-// =====================================================================================
-//
-//       YARP - Yet Another Robotic Platform (c) 2001-2003 
-//
-//                    #Ing. Carlos Beltran#
-//
-//     "Licensed under the Academic Free License Version 1.0"
-// 
-//        Filename:  YARPBabybotSoundGrabber.h
-// 
-//     Description:  
-// 
-//         Version:  $Id: YARPBabybotSoundGrabber.h,v 1.1 2004-07-28 17:17:35 babybot Exp $
-// 
-//          Author:  Ing. Carlos Beltran (Carlos)
-//         Company:  Lira-Lab
-//           Email:  cbeltran@dist.unige.it
-// 
-// =====================================================================================
+/////////////////////////////////////////////////////////////////////////
+///                                                                   ///
+///       YARP - Yet Another Robotic Platform (c) 2001-2004           ///
+///                                                                   ///
+///                    #Carlos Beltran Gonzalez#                     ///
+///                                                                   ///
+///     "Licensed under the Academic Free License Version 1.0"        ///
+///                                                                   ///
+/// The complete license description is contained in the              ///
+/// licence.template file included in this distribution in            ///
+/// $YARP_ROOT/conf. Please refer to this file for complete           ///
+/// information about the licensing of YARP                           ///
+///                                                                   ///
+/// DISCLAIMERS: LICENSOR WARRANTS THAT THE COPYRIGHT IN AND TO THE   ///
+/// SOFTWARE IS OWNED BY THE LICENSOR OR THAT THE SOFTWARE IS         ///
+/// DISTRIBUTED BY LICENSOR UNDER A VALID CURRENT LICENSE. EXCEPT AS  ///
+/// EXPRESSLY STATED IN THE IMMEDIATELY PRECEDING SENTENCE, THE       ///
+/// SOFTWARE IS PROVIDED BY THE LICENSOR, CONTRIBUTORS AND COPYRIGHT  ///
+/// OWNERS "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, ///
+/// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   ///
+/// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO      ///
+/// EVENT SHALL THE LICENSOR, CONTRIBUTORS OR COPYRIGHT OWNERS BE     ///
+/// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   ///
+/// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN ///
+/// CONNECTION WITH THE SOFTWARE.                                     ///
+///                                                                   ///
+/////////////////////////////////////////////////////////////////////////
+
+///
+/// $Id: YARPBabybotSoundGrabber.h,v 1.2 2004-08-30 13:06:56 beltran Exp $
+///
+
 #ifndef __YARPBabybotSoundGrabberh__
 #define __YARPBabybotSoundGrabberh__
 
 #include <yarp/YARPGenericSoundGrabber.h>
 #include <yarp/YARPSoundOnBabybotAdapter.h>
 
-typedef YARPGenericSoundGrabber<YARPSoundOnBabybotAdapter, YARPBabybotSoundGrabberParams> YARPSoundGrabber;
-
-//----------------------------------------------------------------------
-// Note: I define directly the type YARPSoundGrabber and not YARPBabybotSoundDriver. At this level the name of
-// the type can be the same in diferent architecture, is this right?. Thus we avoid a conditional compilation
-// in higher levels (soundgrabber, framegrabber ...etc). 
-//----------------------------------------------------------------------
+typedef YARPGenericSoundGrabber<YARPSoundOnBabybotAdapter, YARPBabybotSoundGrabberParams> YARPBabybotSoundGrabber;
 
 #endif
