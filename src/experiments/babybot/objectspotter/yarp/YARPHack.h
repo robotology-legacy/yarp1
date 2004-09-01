d702f394ddbbabbdf72face50ece28dbdd4f8f83
YARP Developers <yarp0-devel@lists.sourceforge.net>
2004-09-01 19:51:30 +0000

    more on the objectspotter project
    svn path=/trunk/yarp/; revision=1944

---
+++ src/experiments/babybot/objectspotter/yarp/YARPHack.h
@@ -0,0 +1,16 @@
+#ifndef __YARPHack__
+#define __YARPHack__
+
+#define __YARPConfigh__
+
+#define ACE_HAS_DIRENT
+
+#define __LINUXGCC__
+//#include <ace/OS.h>
+//#undef __QNX6__
+#include <yarp/YARPConfigLinux.h>
+#include <yarp/YARPConfigBabybot.h>
+#include <yarp/YARPBabybotVocab.h>
+
+#endif
+
