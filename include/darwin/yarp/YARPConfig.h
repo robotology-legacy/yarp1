6387cf482da19d0a0025e2bd2ad3d247e18a1369
Michele Tavella <tavella.michele@gmail.com>
2004-12-11 17:39:22 +0000

    Darwin support was added
    svn path=/trunk/yarp/; revision=2125

---
+++ include/darwin/yarp/YARPConfig.h
@@ -0,0 +1,29 @@
+
+//
+// $Id: YARPConfig.h,v 1.1 2004-12-11 17:35:18 micheletavella Exp $
+// YARP config file (template).
+//
+//
+
+#ifndef __YARPConfigh__
+#define __YARPConfigh__
+
+//
+// include here your config file and rename to YARPConfig.h
+
+#define YARP_OS_CONFIG DARWIN
+///#define YARP_ROBOT_CONFIG 1
+
+/* OPERATING SYSTEM SUPPORT *******************************************/
+#include <yarp/YARPConfigDarwin.h>
+
+
+/* ROBOT SUPPORT ******************************************************/
+///#ifdef YARP_ROBOT_CONFIG
+
+///#include <yarp/YARPConfigBabybot.h>
+///#include <yarp/YARPBabybotVocab.h>
+
+///#endif
+
+#endif
