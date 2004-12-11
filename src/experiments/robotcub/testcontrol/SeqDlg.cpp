99df9e25922f03dc46626ddace35a3815fa52a11
YARP Developers <yarp0-devel@lists.sourceforge.net>
2004-12-11 00:19:52 +0000

    added a new dialog box
    svn path=/trunk/yarp/; revision=2122

---
+++ src/experiments/robotcub/testcontrol/SeqDlg.cpp
@@ -0,0 +1,108 @@
+// SeqDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "testcontrol.h"
+#include "testcontroldlg.h"
+#include "SeqDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+
+extern YARPHead head;
+extern YARPArm arm;
+
+// CAN bus numbers.
+#define HEAD 0
+#define ARM 1
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CSeqDlg dialog
+
+
+CSeqDlg::CSeqDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CSeqDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CSeqDlg)
+	//}}AFX_DATA_INIT
+
+	int i;
+	for (i = 0; i < SEQUENCE_LEN; i++)
+	{
+		m_s[i] = -1;
+	}
+}
+
+
+void CSeqDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CSeqDlg)
+	//}}AFX_DATA_MAP
+
+	int i;
+	// must be consecutive in resource.h
+	for (i = 0; i < SEQUENCE_LEN; i++)
+	{
+		DDX_Text(pDX, IDC_EDIT1+i, m_s[i]);
+		DDV_MinMaxInt(pDX, m_s[i], -1, N_POSTURES-1);
+	}
+}
+
+
+BEGIN_MESSAGE_MAP(CSeqDlg, CDialog)
+	//{{AFX_MSG_MAP(CSeqDlg)
+	ON_BN_CLICKED(IDC_BUTTON_HIDE, OnButtonHide)
+	ON_BN_CLICKED(IDC_BUTTON_RUN, OnButtonRun)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CSeqDlg message handlers
+
+void CSeqDlg::OnButtonHide() 
+{
+	ShowWindow (SW_HIDE);	
+}
+
+void CSeqDlg::OnButtonRun() 
+{
+	UpdateData (TRUE);
+	CTestControlDlg& p = *(CTestControlDlg *)GetParent();
+	
+	int i;
+	for (i = 0; i < SEQUENCE_LEN; i++)
+	{
+		if (m_s[i] < 0 || m_s[i] >= N_POSTURES)
+			break;
+
+		if (p._headrunning && p._armrunning)
+		{
+			bool finished = false;
+			head.setVelocities (p._headstorev[m_s[i]]);
+			arm.setVelocities (p._armstorev[m_s[i]]);
+			head.setPositions (p._headstore[m_s[i]]);
+			arm.setPositions (p._armstore[m_s[i]]);
+
+			// wait.
+			while (!finished)
+			{
+				finished = head.checkMotionDone();
+				finished &= arm.checkMotionDone();
+				
+				YARPTime::DelayInSeconds (0.1);
+			}
+		}
+		else
+		{
+			MessageBox ("The controllers [arm and head] must be running before using this button", "Error!");
+		}
+	}
+
+	MessageBox ("Sequence execution completed", "Information!");
+}
---
+++ src/experiments/robotcub/testcontrol/SeqDlg.h
@@ -0,0 +1,48 @@
+#if !defined(AFX_SEQDLG_H__B70661E9_D016_4E63_90CE_32BC9AD9E8C9__INCLUDED_)
+#define AFX_SEQDLG_H__B70661E9_D016_4E63_90CE_32BC9AD9E8C9__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// SeqDlg.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CSeqDlg dialog
+
+const int SEQUENCE_LEN = 6;
+
+class CSeqDlg : public CDialog
+{
+// Construction
+public:
+	CSeqDlg(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CSeqDlg)
+	enum { IDD = IDD_DIALOG_SEQUENCE };
+	//}}AFX_DATA
+	int		m_s[SEQUENCE_LEN];
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CSeqDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CSeqDlg)
+	afx_msg void OnButtonHide();
+	afx_msg void OnButtonRun();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_SEQDLG_H__B70661E9_D016_4E63_90CE_32BC9AD9E8C9__INCLUDED_)
