4721e05e2571c9523e1a804770953f71f413ee77
Carlos Beltran-Gonzalez <carlos.beltran@iit.it>
2004-12-27 17:39:31 +0000

    Some minor changes to work with modified YARPGenericControlCard
    svn path=/trunk/yarp/; revision=2161

--- src/libYARP_robot/eurobot/yarp/YARPGALILOnEurobotHeadAdapter.h
+++ src/libYARP_robot/eurobot/yarp/YARPGALILOnEurobotHeadAdapter.h
@@ -61,7 +61,7 @@
 ///
 
 ///
-///  $Id: YARPGALILOnEurobotHeadAdapter.h,v 1.2 2004-12-07 15:44:53 babybot Exp $
+///  $Id: YARPGALILOnEurobotHeadAdapter.h,v 1.3 2004-12-27 17:39:31 beltran Exp $
 ///
 ///
 
@@ -81,6 +81,14 @@
 #else  YARP_BABYBOT_HEAD_ADAPTER_DEBUG(string) YARP_NULL_DEBUG
 #endif
 
+/**
+ * \file YARPGalilOnEurobotAdapter.h This file contains definitions of the control classes
+ * for the Eurobot head acoording to the YARP device driver model.
+ */
+
+/**
+ * _EurobotHead contains the default parameters of the Eurobot head control card(s).
+ */
 namespace _EurobotHead
 {
 	/***************************************************************************
@@ -121,9 +129,29 @@ namespace _EurobotHead
 	const double _maxDAC[_nj] = {9.9, 9.9, 9.9, 9.9};
 }; // namespace
 
+
+/**
+ * YARPEurobotHeadParameters is one of the components required to
+ * specialize the YARPGenericControlBoard template to do something useful.
+ * This class contains parameters that are used during initialization to
+ * bring the head up into a decent state.
+ *
+ * A note on the use of the axis map:
+ * - Each map's entry represents a name of a joint (imagine it as a label).
+ * - Whenever addressing the card directly thus the label is used.
+ * - When addressing software data, initialization file, etc. our index (what we
+ * think of an axis number is used.
+ * - Since the axis map is only used internally this should be of no concern for
+ * the user.
+ *
+ */
 class YARPEurobotHeadParameters
 {
 public:
+	/**
+	 * Default constructor.
+	 * Allocates memory and sets parameters to suitable default values.
+	 */
 	YARPEurobotHeadParameters()
 	{
 		_highPIDs = NULL;
@@ -131,6 +159,7 @@ public:
 		_zeros = NULL;
 		_signs = NULL;
 		_axis_map = NULL;
+		_inv_axis_map = NULL;
 		_encoderToAngles = NULL;
 		_stiffPID = NULL;
 		_maxDAC = NULL;
@@ -152,8 +181,26 @@ public:
 			_stiffPID[i] = _EurobotHead::_stiffPID[i];
 			_maxDAC[i] = _EurobotHead::_maxDAC[i];
 		}
+
+		// invert the axis map.
+		ACE_OS::memset (_inv_axis_map, 0, sizeof(int) * _nj);
+		for (int i = 0; i < _nj; i++)
+		{
+			int j;
+			for (j = 0; j < _nj; j++)
+			{
+				if (_axis_map[j] == i)
+				{
+					_inv_axis_map[i] = j;
+					break;
+				}
+			}
+		}
 	}
 
+	/**
+	 * Destructor.
+	 */
 	~YARPEurobotHeadParameters()
 	{
 		if (_highPIDs != NULL)
@@ -166,6 +213,8 @@ public:
 			delete [] _signs;
 		if (_axis_map != NULL)
 			delete [] _axis_map;
+		if (_inv_axis_map != NULL)
+			delete [] _inv_axis_map;
 		if (_encoderToAngles != NULL)
 			delete [] _encoderToAngles;
 		if (_stiffPID != NULL)
@@ -178,6 +227,13 @@ public:
 			delete [] _limitsMin;
 	}
 
+	/**
+	 * Loads the paramters from a configuration file and allocates memory
+	 * if necessary.
+	 * @param path is the path where the initialization file is located.
+	 * @param init_file is the intitialization file name.
+	 * @return YARP_OK on success, YARP_FAIL otherwise.
+	 */
 	int load(const YARPString &path, const YARPString &init_file)
 	{
 		YARPConfigFile cfgFile;
@@ -217,6 +273,22 @@ public:
 			return YARP_FAIL;
 		if (cfgFile.get("[GENERAL]", "AxisMap", _axis_map, _nj) == YARP_FAIL)
 			return YARP_FAIL;
+
+		// invert the axis map.
+		ACE_OS::memset (_inv_axis_map, 0, sizeof(int) * _nj);
+		for (i = 0; i < _nj; i++)
+		{
+			int j;
+			for (j = 0; j < _nj; j++)
+			{
+				if (_axis_map[j] == i)
+				{
+					_inv_axis_map[i] = j;
+					break;
+				}
+			}
+		}
+
 		if (cfgFile.get("[GENERAL]", "Signs", _signs, _nj) == YARP_FAIL)
 			return YARP_FAIL;
 		if (cfgFile.get("[GENERAL]", "MaxDAC", _maxDAC, _nj) == YARP_FAIL)
@@ -246,7 +318,66 @@ public:
 		return YARP_OK;
 	}
 
+	/**
+	 * Copies an existing parameter instance into this one.
+	 * @param peer is the reference to the object to copy in.
+	 * @return YARP_OK on success, YARP_FAIL otherwise.
+	 */
+	int copy (const YARPEurobotHeadParameters& peer)
+	{
+		_nj = peer._nj;
+
+		if (_nj != 0)
+		{
+			_realloc (_nj);
+
+			memcpy (_highPIDs, peer._highPIDs, sizeof(LowLevelPID) * _nj);
+			memcpy (_lowPIDs, peer._lowPIDs, sizeof(LowLevelPID) * _nj);
+			memcpy (_zeros, peer._zeros, sizeof(double) * _nj);
+			memcpy (_signs, peer._signs, sizeof(double) * _nj);
+			memcpy (_axis_map, peer._axis_map, sizeof(int) * _nj);
+			memcpy (_inv_axis_map, peer._inv_axis_map, sizeof(int) * _nj);
+			memcpy (_encoderToAngles, peer._encoderToAngles, sizeof(double) * _nj);
+			memcpy (_stiffPID, peer._stiffPID, sizeof(int) * _nj);
+			memcpy (_maxDAC, peer._maxDAC, sizeof(double) * _nj);
+			memcpy (_limitsMax, peer._limitsMax, sizeof(double) * _nj);
+			memcpy (_limitsMin, peer._limitsMin, sizeof(double) * _nj);
+		}
+		else
+		{
+			if (_highPIDs != NULL) delete [] _highPIDs;
+			if (_lowPIDs != NULL) delete [] _lowPIDs;
+			if (_zeros != NULL)	delete [] _zeros;
+			if (_signs != NULL)	delete [] _signs;
+			if (_axis_map != NULL) delete [] _axis_map;
+			if (_inv_axis_map != NULL) delete [] _inv_axis_map;
+			if (_encoderToAngles != NULL) delete [] _encoderToAngles;
+			if (_stiffPID != NULL) delete [] _stiffPID;
+			if (_maxDAC != NULL) delete [] _maxDAC;
+			if (_limitsMax != NULL) delete [] _limitsMax;
+			if (_limitsMin != NULL) delete [] _limitsMin;
+
+			_highPIDs = NULL;
+			_lowPIDs = NULL;
+			_zeros = NULL;
+			_signs = NULL;
+			_axis_map = NULL;
+			_inv_axis_map = NULL;
+			_encoderToAngles = NULL;
+			_stiffPID = NULL;
+			_maxDAC = NULL;
+			_limitsMax = NULL;
+			_limitsMin = NULL;
+		}
+
+		return YARP_OK;
+	}
+
 private:
+	/**
+	 * Frees memory and reallocates arrays of the new size.
+	 * @param nj is the new size (number of joints).
+	 */
 	void _realloc(int nj)
 	{
 		if (_highPIDs != NULL)
@@ -259,6 +390,8 @@ private:
 			delete [] _signs;
 		if (_axis_map != NULL)
 			delete [] _axis_map;
+		if (_inv_axis_map != NULL)
+			delete [] _inv_axis_map;
 		if (_encoderToAngles != NULL)
 			delete [] _encoderToAngles;
 		if (_stiffPID != NULL)
@@ -275,6 +408,7 @@ private:
 		_zeros = new double [nj];
 		_signs = new double [nj];
 		_axis_map = new int [nj];
+		_inv_axis_map = new int [nj];
 		_encoderToAngles = new double [nj];
 		_limitsMax = new double [nj];
 		_limitsMin = new double [nj];
@@ -288,6 +422,7 @@ public:
 	double *_zeros;
 	double *_signs;
 	int *_axis_map;
+	int *_inv_axis_map;
 	double *_encoderToAngles;
 	int *_stiffPID;
 	int _nj;
@@ -298,20 +433,46 @@ public:
 	char _mask;
 };
 
+
+/**
+ * YARPGALILOnEurobotHeadAdapter is a specialization of the Galil card device driver
+ * to control the Eurobot head. This class especially implements initialize and
+ * uninitialize while it leaves much of the burden of calling the device driver
+ * to a generic template class called YARPGenericControlBoard.
+ */
 class YARPGALILOnEurobotHeadAdapter : public YARPGalilDeviceDriver
 {
 public:
+	/**
+	 * Default constructor.
+	 */
 	YARPGALILOnEurobotHeadAdapter()
 	{
 		_initialized = false;
 	}
 	
+	/**
+	 * Destructor.
+	 */
 	~YARPGALILOnEurobotHeadAdapter()
 	{
 		if (_initialized)
 			uninitialize();
 	}
-	
+
+	/**
+	 * Initializes the adapter and opens the device driver.
+	 * This is a specific initialization for the Eurobot head. NOTE: that the parameter
+	 * here is not copied and references to it could still be made by the code. Until
+	 * this behavior is correct, the user has to make sure the pointer doesn't become
+	 * invalid during the lifetime of the adapter class (this one). Generally this is true
+	 * for the "generic" templates since they allocate a copy of the parameter class
+	 * internally (and their lifetime is related to that of the adapter).
+	 *
+	 * @param par is a pointer to the class containing the parameters that has
+	 * to be exactly YARPEurobotHeadParameters.
+	 * @return YARP_OK on success, YARP_FAIL otherwise.
+	 */
 	int initialize(YARPEurobotHeadParameters *par)
 	{
 		_parameters = par;
@@ -369,6 +530,10 @@ public:
 		return YARP_OK;
 	}
 
+	/**
+	 * Uninitializes the controller and closes the device driver.
+	 * @return YARP_OK on success, YARP_FAIL otherwise.
+	 */
 	int uninitialize()
 	{
 		if (YARPGalilDeviceDriver::close() != 0)
@@ -377,6 +542,12 @@ public:
 		_initialized = false;
 		return YARP_OK;
 	}
+
+	/**
+	 * Disables simultaneously the controller and the amplifier (these are
+	 * usually two separate commands on control cards).
+	 * @return YARP_OK always.
+	 */
 	int idleMode()
 	{
 		for(int i = 0; i < _parameters->_nj; i++)
@@ -385,11 +556,25 @@ public:
 		return YARP_OK;
 	}
 
+	/**
+	 * Sets the PID values specified in a second set of parameters 
+	 * typically read from the intialization file.
+	 * @param reset if true resets the encoders.
+	 * @return YARP_OK on success, YARP_FAIL otherwise.
+	 */
 	int activateLowPID(bool reset = true)
 	{
 		return activatePID(reset, _parameters->_lowPIDs);
 	}
 
+	/**
+	 * Sets the PID values.
+	 * @param reset if true resets the encoder values to zero.
+	 * @param pids is an array of PID data structures. If NULL the values
+	 * contained into an internal variable are used (presumably read from the
+	 * initialization file) otherwise the actual argument is used.
+	 * @return YARP_OK on success, YARP_FAIL otherwise.
+	 */
 	int activatePID(bool reset, LowLevelPID *pids = NULL)
 	{
 		for(int i = 0; i < _parameters->_nj; i++)
@@ -419,6 +604,11 @@ public:
 		return YARP_OK;
 	}
 
+	/**
+	 * Reads the analog values from the control card (ADC values).
+	 * @param val is the array to receive the analog readings.
+	 * @return YARP_FAIL always.
+	 */
 	int readAnalogs(double *val)
 	{
 		int i;
@@ -438,7 +628,13 @@ public:
 		return ret;
 	}
 
-	int calibrate()
+	/**
+	 * Calibrates the control card if needed.
+	 * @param joint is the joint number/function requested to the calibrate method. 
+	 * The default value -1 does nothing.
+	 * @return YARP_OK always.
+	 */
+	int calibrate(int joint = -1)
 	{
 		YARP_BABYBOT_HEAD_ADAPTER_DEBUG(("Starting head calibration routine"));
 		ACE_OS::printf("..done!\n");
