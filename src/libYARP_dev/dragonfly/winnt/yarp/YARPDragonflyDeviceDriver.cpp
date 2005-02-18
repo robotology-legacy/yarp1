56fc8dc1240ac41785b1fa74e82210002cfbe8b4
Matteo Brunettini <matteo.brunettini@iit.it>
2005-02-18 14:55:25 +0000

    New Dragonfly Camera Device Driver
    svn path=/trunk/yarp/; revision=2208

---
+++ src/libYARP_dev/dragonfly/winnt/yarp/YARPDragonflyDeviceDriver.cpp
@@ -0,0 +1,508 @@
+/////////////////////////////////////////////////////////////////////////
+///                                                                   ///
+///       YARP - Yet Another Robotic Platform (c) 2001-2004           ///
+///                                                                   ///
+///                    #Add our name(s) here#                         ///
+///                                                                   ///
+///     "Licensed under the Academic Free License Version 1.0"        ///
+///                                                                   ///
+/// The complete license description is contained in the              ///
+/// licence.template file included in this distribution in            ///
+/// $YARP_ROOT/conf. Please refer to this file for complete           ///
+/// information about the licensing of YARP                           ///
+///                                                                   ///
+/// DISCLAIMERS: LICENSOR WARRANTS THAT THE COPYRIGHT IN AND TO THE   ///
+/// SOFTWARE IS OWNED BY THE LICENSOR OR THAT THE SOFTWARE IS         ///
+/// DISTRIBUTED BY LICENSOR UNDER A VALID CURRENT LICENSE. EXCEPT AS  ///
+/// EXPRESSLY STATED IN THE IMMEDIATELY PRECEDING SENTENCE, THE       ///
+/// SOFTWARE IS PROVIDED BY THE LICENSOR, CONTRIBUTORS AND COPYRIGHT  ///
+/// OWNERS "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, ///
+/// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   ///
+/// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO      ///
+/// EVENT SHALL THE LICENSOR, CONTRIBUTORS OR COPYRIGHT OWNERS BE     ///
+/// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   ///
+/// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN ///
+/// CONNECTION WITH THE SOFTWARE.                                     ///
+///                                                                   ///
+/////////////////////////////////////////////////////////////////////////
+
+///
+///
+///       YARP - Yet Another Robotic Platform (c) 2001-2003 
+///
+///                    #emmebi#
+///
+///     "Licensed under the Academic Free License Version 1.0"
+///
+
+///
+/// $Id: YARPDragonflyDeviceDriver.cpp,v 1.1 2005-02-18 14:55:25 emmebi75 Exp $
+///
+///
+
+
+#include "YARPDragonflyDeviceDriver.h"
+#include <yarp/YARPFrameGrabberUtils.h>
+//=============================================================================
+// FlyCam Includes
+//=============================================================================
+#include "../dd_orig/include/pgrflycapture.h"
+
+///
+double GetTimeAs_mSeconds(void)
+{
+	ACE_Time_Value timev = ACE_OS::gettimeofday ();
+	return double(timev.sec()*1e3) + timev.usec() * 1e-3; 
+}
+
+//
+class DragonflyResources
+{
+public:
+	DragonflyResources (void) : _newFrameMutex(0)/* CONV_MUTEX*/,  _convImgMutex(1) 
+	{
+		// Variables initialization
+		sizeX = 0;
+		sizeY = 0;
+		maxCams = 0;
+		bufIndex = 0;
+		_canPost = false;
+		imageConverted.pData = NULL;
+		_acqStarted = false;
+		_validContext = false;
+	}
+
+	~DragonflyResources () 
+	{ 
+		_uninitialize (); // To be sure - must protected against double calling
+	}
+	
+	// Hardware-dependant variables
+	enum { _num_buffers = 3 };
+	int sizeX;
+	int sizeY;
+	int maxCams;
+	int bufIndex;
+	bool _canPost;
+	bool _acqStarted;
+	bool _validContext;
+	
+	FlyCaptureContext context;
+	FlyCaptureImage imageBuffer[_num_buffers];
+	FlyCaptureImage imageConverted;
+	YARPSemaphore mutexArray[_num_buffers];
+	YARPSemaphore _newFrameMutex;
+	/* CONV_MUTEX*/
+	YARPSemaphore _convImgMutex;
+	
+	inline int _initialize (const DragonflyOpenParameters& params);
+	inline int _uninitialize (void);
+	inline void _cleanBuffers (void);
+
+	inline bool _setBrightness (int value, bool bDefault=false);
+	inline bool _setExposure (int value, bool bDefault=false);
+	inline bool _setWhiteBalance (int redValue, int blueValue, bool bDefault=false);
+	inline bool _setShutter (int value, bool bDefault=false);
+	inline bool _setGain (int value, bool bDefault=false);
+
+protected:
+	inline void _prepareBuffers (void);
+	inline void _destroyBuffers (void);
+
+};
+
+///
+///
+/// full initialize and startup of the grabber.
+inline int DragonflyResources::_initialize (const DragonflyOpenParameters& params)
+{
+	FlyCaptureError   error = FLYCAPTURE_OK;
+
+	// LATER: add a camera_init function
+
+	sizeX = params._size_x;
+	sizeY = params._size_y;
+
+	// Create the context.
+	if (!_validContext)
+	{
+		error = flycaptureCreateContext( &context );
+		if (error != FLYCAPTURE_OK)
+			return YARP_FAIL;
+		_validContext = true;
+		// Initialize the camera.
+		error = flycaptureInitialize( context, params._unit_number );
+		if (error != FLYCAPTURE_OK)
+			return YARP_FAIL;
+	}
+	
+	// Setup Camera Parameters, Magic Numbers :-)
+	_setBrightness(0);
+	_setExposure(300);
+	_setWhiteBalance(20, 50); 
+	_setShutter(320);	// x * 0.0625 = 20 mSec = 50 Hz
+	_setGain(700);		// x * -0.0224 = -11.2dB
+	
+	// Set color reconstruction method
+	error = flycaptureSetColorProcessingMethod(context, FLYCAPTURE_NEAREST_NEIGHBOR_FAST); // Should be an Option
+	if (error != FLYCAPTURE_OK)
+		return YARP_FAIL;
+
+	// Set Acquisition Timeout
+	error = flycaptureSetGrabTimeoutEx(context, 200);
+	if (error != FLYCAPTURE_OK)
+		return YARP_FAIL;
+	// Buffers
+	_prepareBuffers ();
+
+	// Start Acquisition
+	if (!_acqStarted)
+	{
+		error = flycaptureStart(	context, 
+							FLYCAPTURE_VIDEOMODE_640x480Y8,
+							FLYCAPTURE_FRAMERATE_30 );  // Should be an Option ?
+		if (error != FLYCAPTURE_OK)
+			return YARP_FAIL;
+		_acqStarted = true;
+	}
+	
+	return YARP_OK;
+}
+
+inline int DragonflyResources::_uninitialize (void)
+{
+	int i;
+	FlyCaptureError   error = FLYCAPTURE_OK;
+
+	for (i=0; i< _num_buffers; i++)
+		mutexArray[i].Wait();
+	// Stop Acquisition
+	if (_acqStarted)
+	{
+		error = flycaptureStop(context);
+		if (error != FLYCAPTURE_OK)
+			return YARP_FAIL;
+		_acqStarted = false;
+	}
+	// Destroy the context.
+	if (_validContext)
+	{
+		error = flycaptureDestroyContext( context );
+		if (error != FLYCAPTURE_OK)
+			return YARP_FAIL;
+		_validContext = false;
+	}
+	// Deallocate buffers
+	_destroyBuffers();
+	for (i=0; i< _num_buffers; i++)
+		mutexArray[i].Post();
+
+	return YARP_OK;
+}
+
+///
+///
+inline void DragonflyResources::_prepareBuffers(void)
+{
+	if (imageConverted.pData == NULL)
+		imageConverted.pData = new unsigned char[ sizeX * sizeY * 3 ];
+	imageConverted.pixelFormat = FLYCAPTURE_BGR;
+	_cleanBuffers();
+}
+
+inline void DragonflyResources::_cleanBuffers(void)
+{
+	for (int i=0; i< _num_buffers; i++)
+		memset( &imageBuffer[i], 0x0, sizeof( FlyCaptureImage ) );
+}
+inline void DragonflyResources::_destroyBuffers(void)
+{
+	if (imageConverted.pData != NULL)
+		delete [] imageConverted.pData;
+	imageConverted.pData = NULL;
+}
+
+inline bool DragonflyResources::_setBrightness (int value, bool bAuto)
+{
+	FlyCaptureError   error = FLYCAPTURE_OK;
+	error = flycaptureSetCameraProperty(context, FLYCAPTURE_BRIGHTNESS, value, 0, bAuto);
+	if (error == FLYCAPTURE_OK)
+		return true;
+	else 
+		return false;
+}
+
+inline bool DragonflyResources::_setExposure (int value, bool bAuto)
+{
+	FlyCaptureError   error = FLYCAPTURE_OK;
+	error = flycaptureSetCameraProperty(context, FLYCAPTURE_AUTO_EXPOSURE, value, 0, bAuto);
+	if (error == FLYCAPTURE_OK)
+		return true;
+	else 
+		return false;
+}
+
+inline bool DragonflyResources::_setWhiteBalance (int redValue, int blueValue, bool bAuto)
+{
+	FlyCaptureError   error = FLYCAPTURE_OK;
+	error = flycaptureSetCameraProperty(context, FLYCAPTURE_WHITE_BALANCE, redValue, blueValue, bAuto);
+	if (error == FLYCAPTURE_OK)
+		return true;
+	else 
+		return false;
+}
+
+inline bool DragonflyResources::_setShutter (int value, bool bAuto)
+{
+	FlyCaptureError   error = FLYCAPTURE_OK;
+	error = flycaptureSetCameraProperty(context, FLYCAPTURE_SHUTTER,value, 0, bAuto);
+	if (error == FLYCAPTURE_OK)
+		return true;
+	else 
+		return false;
+}
+
+inline bool DragonflyResources::_setGain (int value, bool bAuto)
+{
+	FlyCaptureError   error = FLYCAPTURE_OK;
+	error = flycaptureSetCameraProperty(context, FLYCAPTURE_GAIN, value, 0, bAuto);
+	if (error == FLYCAPTURE_OK)
+		return true;
+	else 
+		return false;
+}
+
+inline DragonflyResources& RES(void *res) { return *(DragonflyResources *)res; }
+
+///
+///
+YARPDragonflyDeviceDriver::YARPDragonflyDeviceDriver(void) : YARPDeviceDriver<YARPNullSemaphore, YARPDragonflyDeviceDriver>(FCMDNCmds)
+{
+	system_resources = (void *) new DragonflyResources;
+	ACE_ASSERT (system_resources != NULL);
+
+	/// for the IOCtl call.
+	m_cmds[FCMDAcquireBuffer] = &YARPDragonflyDeviceDriver::acquireBuffer;
+	m_cmds[FCMDReleaseBuffer] = &YARPDragonflyDeviceDriver::releaseBuffer;
+	m_cmds[FCMDWaitNewFrame] = &YARPDragonflyDeviceDriver::waitOnNewFrame;
+	m_cmds[FCMDGetSizeX] = &YARPDragonflyDeviceDriver::getWidth;
+	m_cmds[FCMDGetSizeY] = &YARPDragonflyDeviceDriver::getHeight;
+	m_cmds[FCMDSetBright] = &YARPDragonflyDeviceDriver::setBrightness;
+	m_cmds[FCMDSetHue] = &YARPDragonflyDeviceDriver::setHue;
+	m_cmds[FCMDSetContrast] = &YARPDragonflyDeviceDriver::setContrast;
+	m_cmds[FCMDSetSatU] = &YARPDragonflyDeviceDriver::setSatU;
+	m_cmds[FCMDSetSatV] = &YARPDragonflyDeviceDriver::setSatV;
+	m_cmds[FCMDSetLNotch] = &YARPDragonflyDeviceDriver::setLNotch;
+	m_cmds[FCMDSetLDec] = &YARPDragonflyDeviceDriver::setLDec;
+	m_cmds[FCMDSetCrush] = &YARPDragonflyDeviceDriver::setCrush;
+
+}
+
+YARPDragonflyDeviceDriver::~YARPDragonflyDeviceDriver()
+{
+	if (system_resources != NULL)
+		delete (DragonflyResources *)system_resources;
+	system_resources = NULL;
+}
+
+///
+///
+int YARPDragonflyDeviceDriver::open (void *res)
+{
+	DragonflyResources& d = RES(system_resources);
+	int ret = d._initialize (*(DragonflyOpenParameters *)res);
+	YARPScheduler::setHighResScheduling ();
+	Begin ();
+
+	return ret;
+}
+
+int YARPDragonflyDeviceDriver::close (void)
+{
+	DragonflyResources& d = RES(system_resources);
+
+	End ();	/// stops the thread first (joins too).
+
+	int ret = d._uninitialize ();
+
+	return ret;
+}
+
+///
+///
+/// acquisition thread for real!
+void YARPDragonflyDeviceDriver::Body (void)
+{
+	DragonflyResources& d = RES(system_resources);
+
+	FlyCaptureError   error = FLYCAPTURE_OK;
+
+	d._cleanBuffers();
+	d.bufIndex = 0;
+	d._canPost = true;
+
+	int nImages = 0;
+	double startTime;
+	startTime = GetTimeAs_mSeconds();
+	int lostFrames = 0;
+
+	ACE_DEBUG ((LM_DEBUG, "\n[DragonFly Driver] Acquisition thread starting... "));
+	while (!IsTerminated())	
+	{
+		d.bufIndex++;
+	
+		if (d.bufIndex == d._num_buffers)
+				d.bufIndex = 0;
+		
+		if ((d.mutexArray[d.bufIndex]).PollingWait() == 1)
+		{
+
+			error = flycaptureGrabImage2( d.context, &(d.imageBuffer[d.bufIndex]) );
+			if (d._canPost)
+			{
+				d._canPost = false;
+				d._newFrameMutex.Post();
+			}
+			
+			(d.mutexArray[d.bufIndex]).Post();
+			if (error != FLYCAPTURE_OK)
+			{
+				ACE_DEBUG ((LM_DEBUG, "\n[DragonFly Driver] ERROR: It's likely that the acquisition failed, returning. "));
+				AskForEnd();
+			}
+			nImages++;
+		}
+		else
+		{
+			ACE_DEBUG ((LM_DEBUG, "\n[DragonFly Driver] WARNING: Frame lost. "));
+			lostFrames++;
+		}
+	}
+	
+	double stopTime;
+	stopTime = GetTimeAs_mSeconds();
+	unsigned int delay = int(stopTime - startTime);
+	unsigned int avTime = delay / nImages;
+	double fps = 1000 * (1/double(avTime));
+	char str[256];
+	sprintf(str,"\n[DragonFly Driver] Acquired %u frames(s) in %ums (average time %ums per image, %.2ffps). ", nImages, delay, avTime, fps);
+	ACE_DEBUG ((LM_DEBUG, str));
+	sprintf(str,"\n[DragonFly Driver] %u lost Frame(s). ");
+	ACE_DEBUG ((LM_DEBUG, "\n[DragonFly Driver] Acquisition thread returning... "));
+}
+
+int YARPDragonflyDeviceDriver::acquireBuffer (void *buffer)
+{
+	DragonflyResources& d = RES(system_resources);
+
+	FlyCaptureError   error = FLYCAPTURE_OK;
+
+	int reqBuffer = d.bufIndex - 1;
+	if (reqBuffer < 0)
+		reqBuffer = d._num_buffers - 1;
+
+	(d.mutexArray[reqBuffer]).Wait ();
+	/* CONV_MUTEX*/
+	d._convImgMutex.Wait();
+	 
+	FlyCaptureImage *pSrc;
+	FlyCaptureImage *pDst;
+
+	pDst = &(d.imageConverted);
+	pSrc = &(d.imageBuffer[reqBuffer]);
+
+	error = flycaptureConvertImage( d.context, pSrc, pDst );
+
+	(d.mutexArray[reqBuffer]).Post ();
+
+	if (error != FLYCAPTURE_OK)
+		return YARP_FAIL;
+	
+	unsigned char * pData = (*(unsigned char **)buffer);
+
+	// Copy or just reference??
+	//memcpy (pData, d.imageConverted.pData, (d.sizeX * d.sizeY * 3) );
+	(*(unsigned char **)buffer)= d.imageConverted.pData;
+	
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::releaseBuffer (void *cmd)
+{
+	DragonflyResources& d = RES(system_resources);
+
+	d._canPost = true;
+	/* CONV_MUTEX*/ 
+	d._convImgMutex.Post();
+	
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::waitOnNewFrame (void *cmd)
+{
+	DragonflyResources& d = RES(system_resources);
+	
+	d._newFrameMutex.Wait ();
+	
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::getWidth (void *cmd)
+{
+	*(int *)cmd = RES(system_resources).sizeX;
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::getHeight (void *cmd)
+{
+	*(int *)cmd = RES(system_resources).sizeY;
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::setBrightness (void *cmd)
+{
+	/// RES(system_resources)._setBrightness(*cmd);
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::setHue (void *cmd)
+{
+	/// RES(system_resources)._setHue(*cmd);
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::setContrast (void *cmd)
+{
+	/// RES(system_resources)._setContrast(*cmd);
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::setSatU (void *cmd)
+{
+	/// RES(system_resources)._setSatU(*cmd);
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::setSatV (void *cmd)
+{
+	/// RES(system_resources)._setSatV(*cmd);
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::setLNotch (void *cmd)
+{
+	/// RES(system_resources)._setLNotch(*cmd);
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::setLDec (void *cmd)
+{
+	/// RES(system_resources)._setLDec(*cmd);
+	return YARP_OK;
+}
+
+int YARPDragonflyDeviceDriver::setCrush (void *cmd)
+{
+	/// RES(system_resources)._setCrush(*cmd);
+	return YARP_OK;
+}
---
+++ src/libYARP_dev/dragonfly/winnt/yarp/YARPDragonflyDeviceDriver.h
@@ -0,0 +1,219 @@
+/////////////////////////////////////////////////////////////////////////
+///                                                                   ///
+///       YARP - Yet Another Robotic Platform (c) 2001-2004           ///
+///                                                                   ///
+///                    #Add our name(s) here#                         ///
+///                                                                   ///
+///     "Licensed under the Academic Free License Version 1.0"        ///
+///                                                                   ///
+/// The complete license description is contained in the              ///
+/// licence.template file included in this distribution in            ///
+/// $YARP_ROOT/conf. Please refer to this file for complete           ///
+/// information about the licensing of YARP                           ///
+///                                                                   ///
+/// DISCLAIMERS: LICENSOR WARRANTS THAT THE COPYRIGHT IN AND TO THE   ///
+/// SOFTWARE IS OWNED BY THE LICENSOR OR THAT THE SOFTWARE IS         ///
+/// DISTRIBUTED BY LICENSOR UNDER A VALID CURRENT LICENSE. EXCEPT AS  ///
+/// EXPRESSLY STATED IN THE IMMEDIATELY PRECEDING SENTENCE, THE       ///
+/// SOFTWARE IS PROVIDED BY THE LICENSOR, CONTRIBUTORS AND COPYRIGHT  ///
+/// OWNERS "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, ///
+/// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   ///
+/// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO      ///
+/// EVENT SHALL THE LICENSOR, CONTRIBUTORS OR COPYRIGHT OWNERS BE     ///
+/// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   ///
+/// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN ///
+/// CONNECTION WITH THE SOFTWARE.                                     ///
+///                                                                   ///
+/////////////////////////////////////////////////////////////////////////
+
+///
+///
+///       YARP - Yet Another Robotic Platform (c) 2001-2003 
+///
+///                    #emmebi#
+///
+///     "Licensed under the Academic Free License Version 1.0"
+///
+
+///
+/// $Id: YARPDragonflyDeviceDriver.h,v 1.1 2005-02-18 14:55:25 emmebi75 Exp $
+///
+///
+
+#ifndef __YARPDragonflyDeviceDriverh__
+#define __YARPDragonflyDeviceDriverh__
+
+//=============================================================================
+// YARP Includes
+//=============================================================================
+
+#include <yarp/YARPConfig.h>
+#include <ace/config.h>
+#include <ace/OS.h>
+#include <ace/Sched_Params.h>
+#include <yarp/YARPThread.h>
+#include <yarp/YARPSemaphore.h>
+#include <yarp/YARPDeviceDriver.h>
+#include <yarp/YARPScheduler.h>
+
+
+/**
+ * \file YARPPicoloDeviceDriver.h device driver for managing the Dragonfly IEEE-1394 Camera.
+ */
+
+/**
+ * Structure for defining the open() parameters of the camera.
+ */
+struct DragonflyOpenParameters
+{
+	/**
+	 * Constructor. Add here the parameters for the open().
+	 */
+	DragonflyOpenParameters()
+	{
+		// parameters initialization
+		_unit_number = 0;
+		_size_x = 0;
+		_size_y = 0;
+		_video_type = 0;
+	}
+
+	// Parameters
+	unsigned int _unit_number;
+	unsigned int _size_x;
+	unsigned int _size_y;
+	unsigned int _video_type;
+};
+
+class YARPDragonflyDeviceDriver : public YARPDeviceDriver<YARPNullSemaphore, YARPDragonflyDeviceDriver>,public YARPThread
+{
+private:
+	YARPDragonflyDeviceDriver(const YARPDragonflyDeviceDriver&);
+	void operator=(const YARPDragonflyDeviceDriver&);
+
+public:
+	/**
+	 * Constructor.
+	 */
+	YARPDragonflyDeviceDriver();
+
+	/**
+	 * Destructor.
+	 */
+	virtual ~YARPDragonflyDeviceDriver();
+
+	/**
+	 * Opens the device driver. For the meaning of parameters see DragonflyOpenParameters.
+	 * @param p is a pointer to the open arguments.
+	 * @return returns YARP_OK on success.
+	 */
+	virtual int open(void *d);
+
+	/**
+	 * Closes the device driver.
+	 * @return returns YARP_OK on success.
+	 */
+	virtual int close(void);
+
+// TO REMOVE IN FINAL VERSION;/
+	//protected:
+	/**
+	 * Locks the current image buffer.
+	 * @param buffer is a pointer to the buffer address (i.e. a double pointer).
+	 * @return YARP_OK if successful.
+	 */
+	virtual int acquireBuffer(void *buffer);
+
+	/**
+	 * Releases the current image buffer.
+	 * @param cmd is not used.
+	 * @return YARP_OK if successful.
+	 */
+	virtual int releaseBuffer(void *cmd);
+
+	/**
+	 * Waits on a new frame. An event is signaled when a new frame is acquired,
+	 * the calling thread waits efficiently on this event.
+	 * @param cmd is not used.
+	 * @return YARP_OK on success.
+	 */
+	virtual int waitOnNewFrame (void *cmd);
+
+	/**
+	 * Returns the width of the acquired image.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int getWidth(void *cmd);
+
+	/**
+	 * Returns the height of the acquired image.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int getHeight(void *cmd);
+
+	/**
+	 * Sets the average image brightness. UNIMPLEMENTED.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int setBrightness (void *cmd);
+
+	/**
+	 * Sets the acquisition hue. UNIMPLEMENTED.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int setHue (void *cmd);
+
+	/**
+	 * Sets the acquisition contrast. UNIMPLEMENTED.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int setContrast (void *cmd);
+
+	/**
+	 * Sets the gain of the amplifier on the U chroma channel. UNIMPLEMENTED.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int setSatU (void *cmd);
+
+	/**
+	 * Sets the gain of the amplifier on the V chroma channel. UNIMPLEMENTED.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int setSatV (void *cmd);
+
+	/**
+	 * Enables the notch filter (bt848 hardware). UNIMPLEMENTED.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int setLNotch (void *cmd);
+
+	/**
+	 * Enables the decimation filter (bt848 hardware). UNIMPLEMENTED.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int setLDec (void *cmd);
+
+	/**
+	 * Enables the crush filter (bt848 hardware). UNIMPLEMENTED.
+	 * @param cmd is a pointer to an integer.
+	 * @return YARP_OK on success.
+	 */
+	virtual int setCrush (void *cmd);
+
+protected:
+	void *system_resources;
+
+	virtual void Body(void);
+};
+
+
+#endif
