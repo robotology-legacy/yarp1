19f9d7b155c48e1fef390f495a6dcc44cdcd0d37
YARP Developers <yarp0-devel@lists.sourceforge.net>
2005-06-20 10:31:52 +0000

    new files into visualattention
    svn path=/trunk/yarp/; revision=2375

---
+++ src/experiments/babybot/visualattention/YARPGraph.h
@@ -0,0 +1,9 @@
+#ifndef __YARPGRAPH__
+#define __YARPGRAPH__
+
+namespace YARPGraph {
+	void remove(bool *matr, int dim, int index);
+	void remove(bool *connGraph, int dimCG, char* list, int dimL);
+};
+
+#endif
