198fe58a914a6eb73a3d906e4da49ba471ff017c
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-01-05 19:40:14 +0000

    forgot to commit this file earlier
    svn path=/trunk/yarp/; revision=2707

---
+++ src/java/src/yarp/os/CarrierShiftStream.java
@@ -0,0 +1,93 @@
+
+package yarp.os;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+// moving away from using SocketShiftStream
+
+class CarrierShiftStream implements ShiftStream {
+
+    Carrier current;
+
+
+    CarrierShiftStream(Socket socket) {
+	try {
+	    current = chooseCarrier("tcp");
+	    ((TcpCarrier)current).open(socket);
+	} catch (IOException e) {
+	    System.err.println("failed to open CarrierShiftStream");
+	}
+    }
+    
+    private static Carrier[] delegates = { 
+	new TcpCarrier(),
+	new UdpCarrier(),
+	new McastCarrier()
+    };
+
+    public static Carrier chooseCarrier(String name) {
+	/*
+	for (Iterator it = delegates.iterator(); it.hasNext(); ) {
+	    Carrier c = (Carrier)it.next();
+	    if (name.equals(c.getName())) {
+		return c;
+	    }
+	}
+	*/
+	for (int i=0; i<delegates.length; i++) {
+	    Carrier c = delegates[i];
+	    if (name.equals(c.getName())) {
+		return c;
+	    }
+	}
+	return null;
+    }
+
+    public static Carrier chooseCarrier(int specifier) {
+	/*
+	for (Iterator it = delegates.iterator(); it.hasNext(); ) {
+	    Carrier c = (Carrier)it.next();
+	    if (specifier == c.getSpecifier()) {
+		return c;
+	    }
+	}
+	*/
+	for (int i=0; i<delegates.length; i++) {
+	    Carrier c = delegates[i];
+	    if (specifier == c.getSpecifier()) {
+		return c;
+	    }
+	}
+
+	return null;
+    }
+
+
+    public InputStream getInputStream() throws IOException {
+	return current.getInputStream();
+    }
+
+    public OutputStream getOutputStream() throws IOException {
+	return current.getOutputStream();
+    }
+
+    public void close() throws IOException {
+	current.close();
+    }
+
+    public void become(String carrier, Address address) throws IOException {
+	Carrier next = chooseCarrier(carrier);
+	current.close();
+	next.open(address,current);
+	current = next;
+    }
+
+    public Address getAddress() throws IOException {
+	return current.getLocalAddress();
+    }
+}
+
+
+
