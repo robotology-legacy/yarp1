8738d1c57b864fe6a538d4a040e4a08866d50fe3
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-01-30 22:09:58 +0000

    minor updates
    svn path=/trunk/yarp/; revision=2871

--- src/libYARP_OS2/src-test/TcpFaceTest.cpp
+++ src/libYARP_OS2/src-test/TcpFaceTest.cpp
@@ -14,8 +14,11 @@ private:
   private:
     TcpFaceTest& owner;
     TcpFace face;
+    bool closed;
   public:
-    Listener(TcpFaceTest& owner) : owner(owner) {}
+    Listener(TcpFaceTest& owner) : owner(owner) { 
+      closed = false; 
+    }
 
     ~Listener() {
       //ACE_OS::printf("Listener destroyed\n");
@@ -23,16 +26,22 @@ private:
 
     virtual void run() {
       face.open(Address("localhost",9999,"tcp"));
-      //ACE_OS::printf("Listener waiting\n");
-      try {
-	InputProtocol *ip = face.read();
-	//ACE_OS::printf("Listener got something\n");
-	if (ip!=NULL) {
-	  ip->close();
-	  delete ip;
+      bool done = false;
+      while (!done) {
+	try {
+	  ACE_OS::printf("Listener waiting\n");
+	  InputProtocol *ip = face.read();
+	  ACE_OS::printf("Listener got something\n");
+	  owner.counter++;
+	  if (ip!=NULL) {
+	    ip->close();
+	    delete ip;
+	  }
+	} catch (IOException e) {
+	  ACE_OS::printf("Listener interrupted -- expected -- %s\n",
+			 e.toString().c_str());
+	  done = true;
 	}
-      } catch (IOException e) {
-	//ACE_OS::printf("Listener interrupted -- expected\n");
       }
     }
 
@@ -43,28 +52,60 @@ private:
 
 
 public:
+  int counter;
+
+  TcpFaceTest() {
+    counter = 0;
+  }
+
   virtual String getName() { return "TcpFaceTest"; }
 
   void testListenStartStop() {
     report(0,"testing starting/stopping tcp listening...");
     int tct = Thread::getCount();
-    for (int i=0; i<1; i++) {
+    for (int i=0; i<3; i++) {
       Listener listen(*this);
       listen.start();
-      //checkEqual(Thread::getCount(),tct+1,"thread count");
+      checkEqual(Thread::getCount(),tct+1,"thread count");
       Time::delay(0.33);
-      //ACE_OS::printf("trying to stop listener\n");
       listen.close();
-      //ACE_OS::printf("did close, joining...\n");
-      //Time::delay(3);
       listen.join();
-      //ACE_OS::printf("stopped\n");
     }
   }
 
-  void testListen() {
-    report(0,"testing tcp listening - relies on free ports...");
+  void testConnect() {
+    report(0,"testing connecting...");
+    try {
+      int ct = counter;
+      TcpFace face;
+      Listener listen(*this);
+      listen.start();
+      Time::delay(0.33);
+
+      int repeats = 3;
+      for (int i=0; i<repeats; i++) {
+	ACE_OS::printf("trying to write\n");
+	OutputProtocol *out = face.write(Address("localhost",9999,"tcp"));
+	Time::delay(0.5);
+	out->close();
+	delete out;
+	out = NULL;
+      }
+
+      listen.close();
+      listen.join();
+      checkEqual(counter,ct+repeats,"connection count check");
+
+    } catch (IOException e) {
+      report(1,e.toString() + " <<< got exception");
+    }
+  }
+
+
     /*
+
+  void testListen() {
+    report(0,"testing tcp listening...");
     try {
       TcpFace face;
       ACE_OS::printf("waiting\n");
@@ -81,33 +122,25 @@ public:
     } catch (IOException e) {
       report(1,e.toString() + " <<< got exception");
     }
-    */
   }
+    */
 
-  void testConnect() {
-    /*
-    try {
-      TcpFace ff;
-      OutputProtocol *out = ff.write(Address("localhost",10002,"tcp"));
-      Route route("/from","/to","text");
-      out->open(route);
-      BufferedBlockWriter bw;
-      bw.appendLine("d");
-      bw.appendLine("0 \"Hello World\"");
-      out->write(bw);
-      out->close();
-      delete out;
-    } catch (IOException e) {
-      ACE_DEBUG((LM_ERROR,"tcp skipped, issue: %s", e.toString().c_str()));
-    }
-    */
-  }
+  /*
+    // actual content on a connection:
+    Route route("/from","/to","text");
+    out->open(route);
+    BufferedBlockWriter bw;
+    bw.appendLine("d");
+    bw.appendLine("0 \"Hello World\"");
+    out->write(bw);
+  */
+
 
   virtual void runTests() {
     report(0,"Notice - tcp listening tests rely on port 9999 being free...");
     testListenStartStop();
+    testConnect();
     //testListen();
-    //testConnect();
   }
 };
 
