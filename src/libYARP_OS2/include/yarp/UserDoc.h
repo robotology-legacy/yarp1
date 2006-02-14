7d931f8c648f0a5b33b44ad4b13c182eed641690
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2006-02-14 17:34:26 +0000

    adding some front page doxygen
    svn path=/trunk/yarp/; revision=2994

---
+++ src/libYARP_OS2/include/yarp/UserDoc.h
@@ -0,0 +1,40 @@
+#ifndef _YARP2_USERDOC_
+#define _YARP2_USERDOC_
+
+
+/**
+ * @mainpage User Documentation for the YARP OS library
+ *
+ * @section intro_sec Introduction
+ *
+ * Welcome to the YARP OS library.  This library interfaces
+ * with your operating system and provides some basic services.
+ * We give you
+ * \link yarp::os::Thread Thread\endlink, 
+ * \link yarp::os::Semaphore Semaphore\endlink, and 
+ * \link yarp::os::Time Time\endlink
+ * services just in case
+ * you don't have them, but our main goal is to give you easy network
+ * communication using the YARP 
+ * \link yarp::os::Port Port\endlink
+ *Network.
+ *
+ * @section install_sec What is a Port?
+ *
+ * A \link yarp::os::Port Port\endlink is an object that can read and
+ * write values to peer objects spread throughout a network of
+ * computers.  You can create them in your program, and then add and
+ * remove connections either from that program, from the command line,
+ * or from another program.
+ * Ports are specialized for streaming communication, such as camera
+ * images or motor commands.  You can switch network protocols for any
+ * or all your connections without changing a line of code.
+ *
+ * @section example_sec Give me an example!
+ *
+ * Yes, I should do that, shouldn't I...
+ *
+ */
+
+
+#endif
