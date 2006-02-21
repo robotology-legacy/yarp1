5048428dd3962fdb5dcb4da533620e1eb682b915
YARP Developers <yarp0-devel@lists.sourceforge.net>
2006-02-21 11:41:14 +0000

    no message
    svn path=/trunk/yarp/; revision=3016

--- scripts/babybot/daemons/touchSlay.js
+++
@@ -1,9 +0,0 @@
-//
-// this script WON'T work. for some strange reason, "on" works fine,
-// but "slay" says @RPC server unavailable. in order to kill touch.exe
-// get a console on oceanus and manually kill.
-//
-
-// var WshShell = WScript.CreateObject("WScript.Shell");
-
-// WshShell.Run ("slay -n oceanus touch.exe", 7, true);
