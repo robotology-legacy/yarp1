48252e87815275551ce70d47c333468858ca4637
YARP Developers <yarp0-devel@lists.sourceforge.net>
2006-02-26 13:06:12 +0000

    no message
    svn path=/trunk/yarp/; revision=3060

--- scripts/babybot/vision/handtrackerslay.js
+++ scripts/babybot/vision/handtrackerslay.js
@@ -2,5 +2,6 @@ var WshShell = WScript.CreateObject("WScript.Shell");
 
 WshShell.Run ("handtrackerDisconnect.js", 7, true);
 
-WshShell.Run ("slay -n hades handcolortracke", 7, true);
+WshShell.Run ("slay -n hades handcolortracker.exe", 7, true);
+WshShell.Run ("slay -n hades camview.exe", 7, true);
 //WshShell.Run ("slay -n calliope remotelearn.exe", 7, true);
