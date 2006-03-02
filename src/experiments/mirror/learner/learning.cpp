97b9b1e66ef7973e1c649f79099ca2c26ec8fe6e
Claudio Castellini <claudio.castellini@dlr.de>
2006-03-02 20:43:37 +0000

    first draft of a learner
    svn path=/trunk/yarp/; revision=3072

---
+++ src/experiments/mirror/learner/learning.cpp
@@ -0,0 +1,449 @@
+// learning.cpp : behaviour of learning machines
+//
+
+#include <fstream>
+#include <cmath>
+#include "learning.h"
+
+// ---------------------- learning machine constructor
+
+LearningMachine::LearningMachine(
+  unsigned int DomainSize, unsigned int CodomainSize, unsigned int NumOfExamples, string& MachineFileName
+) :
+  _domainSize(DomainSize), _codomainSize(CodomainSize), _numOfExamples(NumOfExamples),
+  _machineFileName(MachineFileName), _exampleCount(0), _normalExampleCount(0)
+{
+
+	// allocate and clean memory for the examples: samples,
+	_sample = new sample*[_numOfExamples];
+	{ foreach(_numOfExamples,i) {
+		_sample[i] = new sample[_domainSize+1];
+		foreach(_domainSize,k) {
+			_sample[i][k].index = k+1;
+			_sample[i][k].value = 0;
+		}
+		_sample[i][_domainSize].index = -1;
+	} }
+	// and values
+	_value = new double*[_codomainSize];
+	{ foreach(_codomainSize,i) {
+		_value[i] = new double[_numOfExamples];
+		foreach(_numOfExamples,j) _value[i][j] = 0.0;
+	} }
+
+	// allocate the new sample, whose value to predict
+	_newSample = new sample[_codomainSize];
+
+	// the means and stdvs
+	_domainMean = new double[_domainSize];
+	_codomainMean = new double[_codomainSize];
+	_domainStdv = new double[_domainSize];
+	_codomainStdv = new double[_codomainSize];
+
+	// evaluate statistics first time, that is, fill them with a row of 0s
+	evalStats();
+
+}
+
+LearningMachine::~LearningMachine()
+{
+
+	// delete statistics
+	delete[] _codomainStdv;
+	delete[] _domainStdv;
+	delete[] _codomainMean;
+	delete[] _domainMean;
+
+	// delete examples: values
+	{ foreach(_codomainSize,i) delete[] _value[i]; }
+	delete[] _value;
+	// and samples
+	{ foreach(_numOfExamples,i) delete[] _sample[i]; }
+	delete[] _sample;
+
+}
+
+void LearningMachine::reset( void )
+{
+
+	// just reset counters
+	_normalExampleCount = 0;
+	_exampleCount = 0;
+
+}
+
+void LearningMachine::save( void )
+{
+
+	// save means and stdvs
+	string statsFileName = _machineFileName + ".stats";
+	ofstream ofOut(statsFileName.c_str());
+
+	{ foreach(_domainSize,i) ofOut << _domainMean[i] << " " << _domainStdv[i] << endl; }
+	{ foreach(_codomainSize,i) ofOut << _codomainMean[i] << " " << _codomainStdv[i] << endl; }
+
+}
+
+const bool LearningMachine::load( void )
+{
+
+	// load means and stdvs
+	string statsFileName = _machineFileName + ".stats";
+	ifstream ifIn(statsFileName.c_str());
+	if ( ifIn.is_open() == false ) {
+		return false;
+	}
+
+	{ foreach(_domainSize,i) ifIn >> _domainMean[i] >> _domainStdv[i]; }
+	{ foreach(_codomainSize,i) ifIn >> _codomainMean[i] >> _codomainStdv[i]; }
+	
+	return true;
+
+}
+
+void LearningMachine::evalStats()
+{
+
+	// clean means and standard deviations
+	{ foreach(_domainSize,i) _domainMean[i] = 0.0; }
+	{ foreach(_domainSize,i) _domainStdv[i] = 0.0; }
+	{ foreach(_codomainSize,i) _codomainMean[i] = 0.0; }
+	{ foreach(_codomainSize,i) _codomainStdv[i] = 0.0; }
+
+	// evaluate means
+	{ foreach(_exampleCount,i) {
+		{ foreach(_domainSize,j) _domainMean[j] += _sample[i][j].value; }
+		{ foreach(_codomainSize,j) _codomainMean[j] += _value[j][i]; }
+	} }
+	{ foreach(_domainSize,j) _domainMean[j] /= _exampleCount; }
+	{ foreach(_codomainSize,j) _codomainMean[j] /= _exampleCount; }
+
+	// evaluate standard deviations
+	{ foreach(_exampleCount,i) {
+		{ foreach(_domainSize,j) _domainStdv[j] +=
+			(_sample[i][j].value-_domainMean[j])*(_sample[i][j].value-_domainMean[j]); }
+		{ foreach(_codomainSize,j) _codomainStdv[j] +=
+			(_value[j][i]-_codomainMean[j])*(_value[j][i]-_codomainMean[j]); }
+	} }
+	{ foreach(_domainSize,j) _domainStdv[j] = sqrt(_domainStdv[j]/(_exampleCount-1)); }
+	{ foreach(_codomainSize,j) _codomainStdv[j] = sqrt(_codomainStdv[j]/(_exampleCount-1)); }
+
+}
+
+void LearningMachine::normalise( double* value, const double mean, const double stdv )
+{
+
+	// normalisation of a value: subtract the mean and divide by the
+	// standard deviation unless it is zero
+	*value -= mean;
+	if ( stdv != 0 ) {
+		*value /= stdv;
+	}
+
+}
+
+void LearningMachine::unNormalise( double* value, const double mean, const double stdv )
+{
+
+	// un-normalisation of a value: multiply by standard deviation
+	// unless it is zero and add the mean
+	if ( stdv != 0 ) {
+		*value *= stdv;
+	}
+	*value += mean;
+
+}
+
+void LearningMachine::normaliseExamples()
+{
+
+	// un-normalise already normalised examples
+	{ foreach(_normalExampleCount,i) {
+		{ foreach(_domainSize,j) unNormalise( &_sample[i][j].value, _domainMean[j], _domainStdv[j] ); }
+		{ foreach(_codomainSize,j) unNormalise( &_value[j][i], _codomainMean[j], _codomainStdv[j] ); }
+	} }
+
+	// evaluate new means and stdvs
+	evalStats();
+
+	// re-normalise all examples
+	{ foreach(_exampleCount,i) {
+		{ foreach(_domainSize,j) normalise( &_sample[i][j].value, _domainMean[j], _domainStdv[j] ); }
+		{ foreach(_codomainSize,j) normalise( &_value[j][i], _codomainMean[j], _codomainStdv[j] ); }
+	} }
+
+	// now they are all normalised!
+	_normalExampleCount = _exampleCount;
+
+}
+
+// ---------------------- SVM learning machine, plain version
+
+SVMLearningMachine::SVMLearningMachine(
+  unsigned int DomainSize, unsigned int CodomainSize, unsigned int NumOfSamples, string& MachineFileName
+ ) : LearningMachine(DomainSize,CodomainSize,NumOfSamples,MachineFileName)
+{
+
+	// initialise SVM parameters
+	_param.svm_type = EPSILON_SVR;
+	_param.kernel_type = RBF;
+	_param.degree = 3;
+	_param.gamma = 1/(double)_domainSize;
+	_param.coef0 = 0;
+	_param.nu = 0.5;
+	_param.cache_size = 10;
+	_param.C = 1;
+	_param.eps = 1e-3;
+	_param.p = 0.1;
+	_param.shrinking = 1;
+	_param.probability = 0;
+	_param.nr_weight = 0;
+	_param.weight_label = 0;
+	_param.weight = 0;
+
+	// check them. the problem is not required unless we use NU-SVC,
+	// which is never the case for us, so a NULL thing will be OK.
+	const char* error_msg;
+	error_msg = svm_check_parameter( 0, &_param );
+	if ( error_msg != 0 ) {
+		exit(0);
+	}
+
+	// allocate SVM problems
+	_problem = new svm_problem[_codomainSize];
+	// allocate and clean svm models pointers
+	_model = new svm_model*[_codomainSize];
+	{ foreach(_codomainSize,i) _model[i] = 0; }
+
+	// initialise SVM problems:
+	// (1) tell them how many _samples we use
+	{ foreach(_codomainSize,i) _problem[i].l = _numOfExamples; }
+	// (2) link their "y" field to values of the LearningMachine
+	{ foreach(_codomainSize,i) _problem[i].y = &_value[i][0]; }
+	// (3) create their "x"'s and link the "x" field to _samples
+	{ foreach(_codomainSize,i) {
+		_problem[i].x = new svm_node*[_numOfExamples+1];
+		foreach(_numOfExamples,j) _problem[i].x[j] = (svm_node*) &_sample[j][0];
+	} }
+	// there you are. from now on you alter _samples[] and _value[], and
+	// really you are touching the SVM problems.
+
+}
+
+SVMLearningMachine::~SVMLearningMachine()
+{
+
+	// delete models
+	{ foreach(_codomainSize,i) if ( _model[i] != 0 ) svm_destroy_model( _model[i] ); }
+	delete[] _model;
+	// delete problems
+	{ foreach(_codomainSize,i) delete[] _problem[i].x; }
+	delete[] _problem;
+	// delete sample to be predicted
+	delete[] _newSample;
+	// destroy machine parameters
+    svm_destroy_param( &_param );
+
+}
+
+const bool SVMLearningMachine::addExample( const double x[], const double y[] )
+{
+
+	// unless the buffer is full,
+	if ( _exampleCount == _numOfExamples ) {
+		return false;
+	} else {
+		// add the new example, update counter and bail out.
+		foreach(_domainSize,i) {
+			_sample[_exampleCount][i].index = i+1;
+			_sample[_exampleCount][i].value = x[i];
+		}
+		_sample[_exampleCount][_domainSize].index = -1;
+		{ foreach(_codomainSize,i) _value[i][_exampleCount] = y[i]; }
+		_exampleCount++;
+		return true;
+	}
+
+}
+
+void SVMLearningMachine::train()
+{
+
+	// destroy old models
+	{ foreach(_codomainSize,i) if ( _model[i] != 0 ) svm_destroy_model( _model[i] ); }
+
+	// normalise the whole pool of examples
+	normaliseExamples();
+
+	// train new models
+	{ foreach(_codomainSize,i) _model[i] = svm_train( &_problem[i], &_param ); }
+
+}
+	
+void SVMLearningMachine::predictValue( const double x[], double y[] )
+{
+
+	// if models are void, do not even try to predict, but return zeroes
+	if ( _model[0] == 0 ) {
+		foreach(_codomainSize,i) y[i] = 0.0;
+		return;
+	}
+
+	// otherwise, build sample to predict (normalise it first)
+	{ foreach(_domainSize,i) {
+		_newSample[i].index = i+1;
+		_newSample[i].value = x[i];
+		normalise( &_newSample[i].value, _domainMean[i], _domainStdv[i] );
+		}
+		_newSample[_domainSize].index = -1;
+	}
+
+	// and then predict (un-normalise before bailing out)
+	{ foreach(_codomainSize,i) {
+		y[i] = svm_predict(_model[i], (svm_node*)_newSample);
+		unNormalise( &y[i], _codomainMean[i], _codomainStdv[i] );
+	} }
+
+}
+
+void SVMLearningMachine::reset( void )
+{
+
+	// destroy models and reset them to 0
+	{ foreach(_codomainSize,i) {
+		if ( _model[i] != 0 ) svm_destroy_model( _model[i] );
+		_model[i] = 0;
+	} }
+
+	LearningMachine::reset();
+
+}
+
+void SVMLearningMachine::save( void )
+{
+
+	// save models
+	{ foreach(_codomainSize,i) {
+		char modelFileName[255];
+		sprintf(modelFileName, "%s.%d.model", _machineFileName.c_str(), i);
+		svm_save_model(modelFileName, _model[i]);
+	} }
+
+	LearningMachine::save();
+
+}
+
+const bool SVMLearningMachine::load( void )
+{
+
+	// load a (set of) previously saved model. if one of the models
+	// or the stats fail to be found, set all models to 0
+
+	{ foreach(_codomainSize,i) {
+		char modelFileName[255];
+		sprintf(modelFileName, "%s.%d.model", _machineFileName.c_str(), i);
+		_model[i] = svm_load_model(modelFileName);
+		if ( _model[i] == 0 ) {
+			foreach(_codomainSize,j) _model[j] = 0;
+			return false;
+		}
+	} }
+
+	if ( LearningMachine::load() == false ) {
+		foreach(_codomainSize,j) _model[j] = 0;
+		return false;
+	}
+
+	return true;
+
+}
+
+// ---------------------- SVM learning machine, uniform version
+
+UniformSVMLearningMachine::UniformSVMLearningMachine(
+  unsigned int DomainSize, unsigned int CodomainSize, unsigned int NumOfSamples,
+  string& MachineFileName, double Tolerance[] ) :
+  SVMLearningMachine(DomainSize,CodomainSize,NumOfSamples,MachineFileName)
+{
+
+	// allocate and initialise tolerances array
+	_tolerance = new double[_domainSize];
+	foreach(_domainSize,i) _tolerance[i] = Tolerance[i];
+
+}
+
+UniformSVMLearningMachine::~UniformSVMLearningMachine( void )
+{
+
+	// delete tolerances
+	delete[] _tolerance;
+
+}
+
+const bool UniformSVMLearningMachine::addExample( const double x[], const double y[] )
+{
+
+	// if the buffer is full, stop.
+	if ( _exampleCount == _numOfExamples ) {
+		return false;
+	} else {
+		// otherwise, is this example worth adding to the current pool?
+		if ( isExampleWorthAdding( x ) ) {
+			// yes: then add it and then bail out.
+			foreach(_domainSize,i) {
+				_sample[_exampleCount][i].index = i+1;
+				_sample[_exampleCount][i].value = x[i];
+			}
+			_sample[_exampleCount][_domainSize].index = -1;
+			{ foreach(_codomainSize,i) _value[i][_exampleCount] = y[i]; }
+			_exampleCount++;
+		}
+		return true;
+	}
+
+}
+
+const bool UniformSVMLearningMachine::isExampleWorthAdding( const double x[] )
+{
+
+	// so: if the example to be added is "close" to any previous one, don't add it
+	bool closeToMe;
+
+	// compare with non-normalised samples
+	{ foreach_s(_normalExampleCount,_exampleCount,i) {
+		closeToMe = true;
+		foreach(_domainSize,j) {
+			if ( abs(x[j]-_sample[i][j].value) > _tolerance[j] ) {
+				closeToMe = false;
+				break;
+			}
+		}
+		if ( closeToMe ) {
+			return false;
+		}
+	} }
+
+	// normalise and compare with normalised samples
+	double* normal_x = new double[_domainSize];
+	{ foreach(_domainSize,i) {
+		normal_x[i] = x[i];
+		normalise( &normal_x[i], _domainMean[i], _domainStdv[i] );
+	} }
+	{ foreach(_normalExampleCount,i) {
+		closeToMe = true;
+		foreach(_domainSize,j) {
+			if ( abs(normal_x[j]-_sample[i][j].value) > _tolerance[j] ) {
+				closeToMe = false;
+				break;
+			}
+		}
+		if ( closeToMe ) {
+			delete[] normal_x;
+			return false;
+		}
+	} }
+	delete[] normal_x;
+
+	return true;
+
+}
---
+++ src/experiments/mirror/learner/main.cpp
@@ -0,0 +1,267 @@
+// main.cpp : a learning module
+//
+
+// ---------- headers
+
+#include <iostream>
+#include <conio.h>
+
+#include <yarp/YARPConfig.h>
+#include <yarp/YARPPort.h>
+#include <yarp/YARPMath.h>
+#include <yarp/YARPTime.h>
+#include <yarp/YARPParseParameters.h>
+
+#include "learning.h"
+#include "lMCommands.h"
+
+using namespace std;
+
+// ---------- prototypes
+
+void registerPorts( void );
+void unregisterPorts( void );
+void parseCmdLine( int, char** );
+int main( int, char** );
+
+// ---------- global variables
+
+// YARP communication ports: data ports
+YARPOutputPortOf<YVector> data_outport (YARPOutputPort::DEFAULT_OUTPUTS, YARP_TCP);
+YARPInputPortOf<YVector> data_inport (YARPInputPort::DEFAULT_BUFFERS, YARP_TCP);
+// YARP communication ports: cmd ports.
+// must be NO_BUFFERS, otherwise sequentiality is lost -
+// to improve synchronous communication, then, we force .write(true) on this port
+YARPOutputPortOf<int> cmd_outport (YARPOutputPort::DEFAULT_OUTPUTS, YARP_TCP);
+YARPInputPortOf<int> cmd_inport (YARPInputPort::NO_BUFFERS, YARP_TCP);
+
+// port basename and network name
+YARPString portName("learner");
+YARPString netName("default");
+
+// domain and codomain size and number of examples
+int domainSize;
+int codomainSize;
+int numOfExamples;
+
+// if it is a uniform machine
+bool uniformMachine;
+double* tolerance;
+
+// the learning machine
+LearningMachine* learner;
+
+// ---------- procedures
+
+void registerPorts( void )
+{
+
+	YARPString fullPortName;
+
+	// register data output port
+	cout << "Registering data output port..." << endl;
+	fullPortName = "/" + portName + "/o:vec";
+	if ( data_outport.Register(fullPortName.c_str(),netName.c_str()) != YARP_OK ) {
+		cout << "FATAL ERROR: could not register port " << fullPortName << endl;
+		exit(YARP_FAIL);
+	} else {
+		cout << "done." << endl;
+	}
+	
+	// register data input port
+	cout << "Registering data input port..." << endl;
+	fullPortName = "/" + portName + "/i:vec";
+	if ( data_inport.Register(fullPortName.c_str(),netName.c_str()) != YARP_OK ) {
+		cout << "FATAL ERROR: could not register port " << fullPortName << endl;
+		exit(YARP_FAIL);
+	} else {
+		cout << "done." << endl;
+	}
+	
+	// register cmd output port
+	cout << "Registering command output port..." << endl;
+	fullPortName = "/" + portName + "/o:int";
+	if ( cmd_outport.Register(fullPortName.c_str(),netName.c_str()) != YARP_OK ) {
+		cout << "FATAL ERROR: could not register port " << fullPortName << endl;
+		exit(YARP_FAIL);
+	} else {
+		cout << "done." << endl;
+	}
+	
+	// register cmd input port
+	cout << "Registering command input port..." << endl;
+	fullPortName = "/" + portName + "/i:int";
+	if ( cmd_inport.Register(fullPortName.c_str(),netName.c_str()) != YARP_OK ) {
+		cout << "FATAL ERROR: could not register port " << fullPortName << endl;
+		exit(YARP_FAIL);
+	} else {
+		cout << "done." << endl;
+	}
+	
+}
+
+void unregisterPorts( void )
+{
+
+	cmd_inport.Unregister();
+	cmd_outport.Unregister();
+	data_inport.Unregister();
+	data_outport.Unregister();
+
+}
+
+void parseCmdLine( int argc, char** argv )
+{
+
+	// help requested?
+	if ( YARPParseParameters::parse(argc, argv, "-help") ) {
+		cout << "USAGE: " << argv[0] << endl;
+		cout << "  --dom <domain size> --cod <codomain size>" << endl;
+		cout << "  --ex <number of examples>" << endl;
+		cout << "  [--port <port basename>] [--net <network name>]" << endl;
+		cout << "  [--u <tolerance values>]" << endl;
+		exit(YARP_OK);
+	}
+
+	// basename of the ports
+	YARPParseParameters::parse(argc, argv, "-port", portName);
+	// network name
+	YARPParseParameters::parse(argc, argv, "-net", netName);
+	// domain size
+	if( ! YARPParseParameters::parse(argc, argv, "-dom", &domainSize) ) {
+		cout << "FATAL ERROR: must specify domain size." << endl;
+		exit(YARP_FAIL);
+	}
+	// codomain size
+	if( ! YARPParseParameters::parse(argc, argv, "-cod", &codomainSize) ) {
+		cout << "FATAL ERROR: must specify codomain size." << endl;
+		exit(YARP_FAIL);
+	}
+	// number of examples
+	if( ! YARPParseParameters::parse(argc, argv, "-ex", &numOfExamples) ) {
+		cout << "FATAL ERROR: must specify number of examples." << endl;
+		exit(YARP_FAIL);
+	}
+
+	// uniform machine required?
+	if ( YARPParseParameters::parse(argc, argv, "-u") ) {
+		uniformMachine = true;
+		unsigned int index;
+		{ foreach(argc,i) {
+			YARPString argument(argv[i]);
+			if ( argument == "--u" ) { index = i; break; }
+		} }
+		// must be followed by domainSize doubles...
+		if ( argc-index < domainSize ) {
+			cout << "FATAL ERROR: must specify " << domainSize << " tolerance arguments." << endl;
+			exit(YARP_FAIL);
+		}
+		tolerance = new double[domainSize];
+		if ( tolerance == 0 ) {
+			cout << "FATAL ERROR: no memory for the tolerances." << endl;
+			exit(YARP_FAIL);
+		}
+		{ foreach_s(index+1,index+1+domainSize,i) 
+			if ( sscanf(argv[i], "%lf", &tolerance[i-index-1]) != 1 ) {
+				cout << "FATAL ERROR: invalid tolerance value." << endl;
+				exit(YARP_FAIL);
+			}
+		}
+	}
+
+	// show parameters
+	cout << "CMDLINE: port basename is " << portName << "." << endl;
+	cout << "CMDLINE: network name is " << netName << "." << endl;
+	cout << "CMDLINE: mapping R^" << domainSize << " to R^" << codomainSize << ", " << numOfExamples << " examples." << endl;
+	if ( uniformMachine ) {
+		cout << "CMDLINE: uniform machine selected. Tolerances are: ";
+		{ foreach(domainSize,i) cout << tolerance[i] << " "; }
+		cout << endl;
+	}
+
+}
+
+// ---------- main
+
+int main ( int argc, char** argv )
+{
+
+	// parse cmd line
+	parseCmdLine( argc, argv );
+
+	// register ports
+	registerPorts();
+
+	// create learning machine according to cmd line
+	if ( uniformMachine ) {
+		string portNameString(portName.c_str());
+		learner = new UniformSVMLearningMachine(domainSize,codomainSize,numOfExamples,portNameString,tolerance);
+	} else {
+		string portNameString(portName.c_str());
+		learner = new SVMLearningMachine(domainSize,codomainSize,numOfExamples,portNameString);
+	}
+	if ( learner == 0 ) {
+		cout << "FATAL ERROR: no memory for the learning machine." << endl;
+		exit(YARP_FAIL);
+	}
+
+	// ------------ main command parsing loop
+
+	bool goOn = true;
+
+	while ( goOn ) {
+
+		// ----- we've received a command!
+		if ( cmd_inport.Read(false) ) {
+			switch ( cmd_inport.Content() ) {
+			case lMCommand::Quit: // QUIT
+				cmd_outport.Content() = lMCommand::Ok;
+				cmd_outport.Write(true);
+				goOn = false;
+				break;
+			case lMCommand::Reset: // RESET
+				learner->reset();
+				cmd_outport.Content() = lMCommand::Ok;
+				cmd_outport.Write(true);
+				break;
+			default: // unrecognised port command
+				cout << "Unrecognised command from the network." << endl;
+				cmd_outport.Content() = lMCommand::Failed;
+				cmd_outport.Write(true);
+			}
+		}
+
+		// ----- we've read an example!
+		if ( data_inport.Read(false) ) {
+		}
+
+		// ----- any keyboard commands?
+		if ( _kbhit() ) {
+			switch ( _getch() ) {
+			case 'r': // RESET
+				learner->reset();
+				cout << "Machine has been reset." << endl;
+				break;
+			case 'q': // QUIT
+				goOn = false;
+				break;
+			default: // unrecognised keyboard command
+				cout << "Unrecognised keyboard command." << endl;
+				cout << "'r' reset machine" << endl;
+				cout << "'q' quit" << endl << endl;
+			}
+		}
+
+		// wait for some time
+		YARPTime::DelayInSeconds(1.0/25.0);
+
+	} // while ( goOn )
+
+	cout << "Bailing out." << endl;
+	unregisterPorts();
+	if ( uniformMachine ) delete[] tolerance;
+	delete learner;
+
+	return 0;
+
+}
