f571bc649586162bb4b93c192a35349915dfc5e0
Claudio Castellini <claudio.castellini@dlr.de>
2006-04-11 16:37:29 +0000

    no message
    svn path=/trunk/yarp/; revision=3204

--- scripts/mirror/BodyMapSlay.js
+++ scripts/mirror/BodyMapSlay.js
@@ -2,8 +2,6 @@ var WshShell = WScript.CreateObject("WScript.Shell");
 
 WshShell.Run ("BodyMapDisconnect.js", 7, true);
 
-//WshShell.Run ("slay -n eolo BodyMap.exe", 7, true);
-//WshShell.Run ("slay -n eolo learner.exe", 7, true);
 WshShell.Run ("slay -n atlas BodyMap.exe", 7, true);
 WshShell.Run ("slay -n atlas learner.exe", 7, true);
 WshShell.Run ("slay -n atlas mirrorCollector.exe", 7, true);
--- scripts/mirror/TeleOperate.js
+++ scripts/mirror/TeleOperate.js
@@ -1,10 +1,13 @@
 var WshShell = WScript.CreateObject("WScript.Shell");
 
-// launch camview
-WshShell.Run ("on -d -n atlas \"camview.exe --name /TCSaccades/i:img --flip --l --x 1 --y 1 --w 900 --h 900 --net Net1 --p 100\"", 7, true);
-
 // launch mirror collector
 WshShell.Run ("on -d -n atlas \"mirrorCollector\"", 7, true);
 
+// launch camview
+WshShell.Run ("on -d -n atlas \"camview.exe --name /TCSaccades/i:img --flip --x 1 --y 1 --w 700 --h 700 --net Net1 --p 100\"", 7, true);
+
 // launch TeleCtrl
 WshShell.Run ("on -d -n atlas \"TeleCtrl\"", 7, true);
+
+// service camview
+WshShell.Run ("on -d -n atlas \"camview.exe --name /service/i:img --l --flip --x 1 --y 1 --w 700 --h 700 --net Net1 --p 100\"", 7, true);
