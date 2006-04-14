cc941639c288bb5ef47d3de13ac2ffe22d0e6294
Claudio Castellini <claudio.castellini@dlr.de>
2006-04-14 15:01:04 +0000

    no message
    svn path=/trunk/yarp/; revision=3251

--- src/experiments/mirror/GraspCapture/graspCaptureConnect.bat
+++ src/experiments/mirror/GraspCapture/graspCaptureConnect.bat
@@ -1,11 +1,11 @@
-yarp-connect /fakeCollector/o:img0 !/mirrorGrasp/i:img0
-yarp-connect /fakeCollector/o:img1 !/mirrorGrasp/i:img1
-yarp-connect /fakeCollector/o:str  !/mirrorGrasp/i:str
-yarp-connect /mirrorGrasp/o:int      !/fakeCollector/i:int
-yarp-connect /fakeCollector/o:int  !/mirrorGrasp/i:int
+yarp-connect /mirrorCollector/o:img0 !/mirrorGrasp/i:img0
+yarp-connect /mirrorCollector/o:img1 !/mirrorGrasp/i:img1
+yarp-connect /mirrorCollector/o:str  !/mirrorGrasp/i:str
+yarp-connect /mirrorGrasp/o:int      !/mirrorCollector/i:int
+yarp-connect /mirrorCollector/o:int  !/mirrorGrasp/i:int
 
-yarp-connect /fakeCollector/o:img0 /mirrorGrasp/i:img0
-yarp-connect /fakeCollector/o:img1 /mirrorGrasp/i:img1
-yarp-connect /fakeCollector/o:str /mirrorGrasp/i:str
-yarp-connect /mirrorGrasp/o:int /fakeCollector/i:int
-yarp-connect /fakeCollector/o:int /mirrorGrasp/i:int
+yarp-connect /mirrorCollector/o:img0 /mirrorGrasp/i:img0
+yarp-connect /mirrorCollector/o:img1 /mirrorGrasp/i:img1
+yarp-connect /mirrorCollector/o:str /mirrorGrasp/i:str
+yarp-connect /mirrorGrasp/o:int /mirrorCollector/i:int
+yarp-connect /mirrorCollector/o:int /mirrorGrasp/i:int
