f0884c58a515179175d514026bdcd9e4715b420e
YARP Developers <yarp0-devel@lists.sourceforge.net>
2006-07-04 11:23:29 +0000

    no message
    svn path=/trunk/yarp/; revision=3769

--- scripts/babybot/daemons/handrun.js
+++ scripts/babybot/daemons/handrun.js
@@ -1,3 +1,3 @@
 var WshShell = WScript.CreateObject("WScript.Shell");
 
-WshShell.Run ("on -n cronus handcontrol.exe", 1, true);
+WshShell.Run ("on -d -n cronus handcontrol.exe", 7, true);
--- scripts/babybot/vision/handtrackerViewer.js
+++ scripts/babybot/vision/handtrackerViewer.js
@@ -1,4 +1,4 @@
 var WshShell = WScript.CreateObject("WScript.Shell");
 
-WshShell.Run ("on -d -n hades \"camview.exe --name /view008/i:img --p 100 --x 1 --y 1\"", 1, true);
+WshShell.Run ("on -d -n hades \"camview.exe --name /view008/i:img --p 100 --x 1 --y 1\"", 7, true);
 //WshShell.Run ("on -d -n oceanus \"camview.exe --name /view009/i:img --l --p 100 --x 250 --y 1\"", 7, true);
