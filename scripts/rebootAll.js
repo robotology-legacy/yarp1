3c95356325001b444367b877ca0c11689bf9fb25
YARP Developers <yarp0-devel@lists.sourceforge.net>
2006-07-04 10:26:08 +0000

    no message
    svn path=/trunk/yarp/; revision=3764

---
+++ scripts/rebootAll.js
@@ -0,0 +1,17 @@
+var WshShell = WScript.CreateObject("WScript.Shell");
+
+WshShell.Run ("shutdown -n hades", 7, true);
+WshShell.Run ("shutdown -n cariddi", 7, true);
+WshShell.Run ("shutdown -n scilla", 7, true);
+
+WshShell.Run ("shutdown -n circe", 7, true);
+WshShell.Run ("shutdown -n polifemo", 7, true);
+WshShell.Run ("shutdown -n rhea", 7, true);
+
+WshShell.Run ("shutdown -n cronus", 7, true);
+WshShell.Run ("shutdown -n proteus", 7, true);
+WshShell.Run ("shutdown -n thaumas", 7, true);
+WshShell.Run ("shutdown -n oceanus", 7, true);
+
+//WshShell.Run ("shutdown -n pento", 7, true);
+//WshShell.Run ("shutdown -n atlas", 7, true);
---
+++ scripts/shutdownAll.js
@@ -0,0 +1,17 @@
+var WshShell = WScript.CreateObject("WScript.Shell");
+
+WshShell.Run ("_shutdown /T:01 /C \\\\hades", 7, true);
+WshShell.Run ("_shutdown /T:01 /C \\\\cariddi", 7, true);
+WshShell.Run ("_shutdown /T:01 /C \\\\scilla", 7, true);
+
+WshShell.Run ("_shutdown /T:01 /C \\\\circe", 7, true);
+WshShell.Run ("_shutdown /T:01 /C \\\\polifemo", 7, true);
+WshShell.Run ("_shutdown /T:01 /C \\\\rhea", 7, true);
+
+WshShell.Run ("_shutdown /T:01 /C \\\\cronus", 7, true);
+WshShell.Run ("_shutdown /T:01 /C \\\\proteus", 7, true);
+WshShell.Run ("_shutdown /T:01 /C \\\\thaumas", 7, true);
+WshShell.Run ("_shutdown /T:01 /C \\\\oceanus", 7, true);
+
+//WshShell.Run ("_shutdown /T:01 /C \\\\pento", 7, true);
+//WshShell.Run ("_shutdown /T:01 /C \\\\atlas", 7, true);
