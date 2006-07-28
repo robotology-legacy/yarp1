25aa01c8cd565ede5b85b81f1c8ae4c08b4ef040
YARP Developers <yarp0-devel@lists.sourceforge.net>
2006-07-28 09:56:31 +0000

    no message
    svn path=/trunk/yarp/; revision=3958

--- scripts/mirror/BodyMapRun.js
+++ scripts/mirror/BodyMapRun.js
@@ -11,5 +11,6 @@ var WshShell = WScript.CreateObject("WScript.Shell");
 
 // launch mirrorCollector, learner and BodyMap
 WshShell.Run ("mirrorCollector", 7, false);
-WshShell.Run ("learner --dom 3 --cod 4 --ex 3000 --u .2 .2 .2", 1, false);
+//WshShell.Run ("learner --dom 3 --cod 4 --ex 3000 --u .2 .2 .2", 1, false);
+WshShell.Run ("learner --dom 3 --cod 4 --ex 3000", 1, false);
 WshShell.Run ("BodyMap", 1, false);
